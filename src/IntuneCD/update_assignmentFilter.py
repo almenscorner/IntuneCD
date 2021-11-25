4c5b014b9c369fa062b7553999f5b6d08f5862d5
tobias <tobias.almen@outlook.com>
2021-11-25 15:06:39 +0100

    fix for creating filter when it does not exist

--- src/IntuneCD/update_assignmentFilter.py
+++ src/IntuneCD/update_assignmentFilter.py
@@ -48,36 +48,36 @@ def update(path,token):
                     
                     ## Get Filter with query parameter
                     mem_data = makeapirequest(endpoint,token)
+                    filter_value = {}
 
                     ## If Filter exists, continue
                     if mem_data['value']:
-                        filter_value = {}
                         for val in mem_data['value']:
                             if repo_data['displayName'] == val['displayName']:
                                 filter_value = val
-                        if filter_value:
-                            print("-" * 90)
-                            filter_id = filter_value['id']
-                            remove_keys = {'id','createdDateTime','version','lastModifiedDateTime'}
-                            for k in remove_keys:
-                                filter_value.pop(k, None)
+                    if filter_value:
+                        print("-" * 90)
+                        filter_id = filter_value['id']
+                        remove_keys = {'id','createdDateTime','version','lastModifiedDateTime'}
+                        for k in remove_keys:
+                            filter_value.pop(k, None)
 
-                            diff = DeepDiff(filter_value, repo_data, ignore_order=True).get('values_changed',{})
+                        diff = DeepDiff(filter_value, repo_data, ignore_order=True).get('values_changed',{})
                             
-                            ## If any changed values are found, push them to Intune
-                            if diff:
-                                print("Updating Filter: " + repo_data['displayName'] + ", values changed:")
-                                print(*diff.items(), sep='\n')
-                                repo_data.pop("platform", None)
-                                request_data = json.dumps(repo_data)
-                                makeapirequestPatch(endpoint + "/" + filter_id,token,q_param=None,jdata=request_data)
-                            else:
-                                print('No difference found for Filter: ' + repo_data['displayName'])
-
-                        ## If Filter does not exist, create it
+                        ## If any changed values are found, push them to Intune
+                        if diff:
+                            print("Updating Filter: " + repo_data['displayName'] + ", values changed:")
+                            print(*diff.items(), sep='\n')
+                            repo_data.pop("platform", None)
+                            request_data = json.dumps(repo_data)
+                            makeapirequestPatch(endpoint + "/" + filter_id,token,q_param=None,jdata=request_data)
                         else:
-                            print("-" * 90)
-                            print("Assignment filter not found, creating filter: " + repo_data['displayName'])
-                            request_json = json.dumps(repo_data)
-                            post_request = makeapirequestPost(endpoint,token,q_param=None,jdata=request_json,status_code=201)
-                            print("Assignemnt filter created with id: " + post_request['id'])
+                            print('No difference found for Filter: ' + repo_data['displayName'])
+
+                    ## If Filter does not exist, create it
+                    else:
+                        print("-" * 90)
+                        print("Assignment filter not found, creating filter: " + repo_data['displayName'])
+                        request_json = json.dumps(repo_data)
+                        post_request = makeapirequestPost(endpoint,token,q_param=None,jdata=request_json,status_code=201)
+                        print("Assignemnt filter created with id: " + post_request['id'])
