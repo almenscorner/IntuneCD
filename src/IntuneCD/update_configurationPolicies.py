d80388325b8c42dbe02738f5d0d9fc1a878e7ffb
Tobias Almén <almenscorner@outlook.com>
2022-03-04 15:48:59 +0100

    Fix q param

--- src/IntuneCD/update_configurationPolicies.py
+++ src/IntuneCD/update_configurationPolicies.py
@@ -89,6 +89,7 @@ def update(path, token, assignment=False):
                                 print(
                                     f"New Value: {new_val}, Old Value: {old_val}")
                             request_data = json.dumps(repo_data)
+                            q_param = None
                             makeapirequestPut(endpoint + "/" + data['value']['id'], token,q_param,request_data,status_code=204)
                         else:
                             print(
