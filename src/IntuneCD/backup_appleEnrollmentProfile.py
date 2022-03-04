00bc0c8243628c1b7c37488a7d81c5f125a7bc9f
Tobias Almén <almenscorner@outlook.com>
2022-03-04 11:25:46 +0100

    Batch enrollment profiles for all accounts

--- src/IntuneCD/backup_appleEnrollmentProfile.py
+++ src/IntuneCD/backup_appleEnrollmentProfile.py
@@ -19,40 +19,40 @@ import yaml
 
 from .clean_filename import clean_filename
 from .graph_request import makeapirequest
+from .graph_batch import batch_request
 
 ## Set MS Graph endpoint
 endpoint = "https://graph.microsoft.com/beta/deviceManagement/depOnboardingSettings/"
 
 ## Get all Apple Enrollment Profiles and save them in specified path
-
-
 def savebackup(path, output, token):
     configpath = path+"/"+"Enrollment Profiles/Apple/"
     data = makeapirequest(endpoint, token)
 
+    profile_ids = []
     for profile in data['value']:
-        profile_data = makeapirequest(
-            endpoint + profile['id'] + '/enrollmentProfiles', token)
-        for p in profile_data['value']:
-            p_data = makeapirequest(
-                endpoint + profile['id'] + '/enrollmentProfiles/' + p['id'], token)
-            pid = p_data['id']
-            remove_keys = {'id', 'createdDateTime', 'version',
-                           'lastModifiedDateTime', 'isDefault'}
-            for k in remove_keys:
-                p_data.pop(k, None)
+        profile_ids.append(profile['id'])
 
-            print("Backing up Apple enrollment profile: " + p['displayName'])
+    batch_profile_data = batch_request(profile_ids,'deviceManagement/depOnboardingSettings/','/enrollmentProfiles',token)
+
+    for profile in batch_profile_data:
+        for value in profile['value']:
+            remove_keys = {'id', 'createdDateTime', 'version',
+                        'lastModifiedDateTime', 'isDefault'}
+            for k in remove_keys:
+                value.pop(k, None)
+
+            print("Backing up Apple enrollment profile: " + value['displayName'])
             if os.path.exists(configpath) == False:
                 os.makedirs(configpath)
 
             ## Get filename without illegal characters
-            fname = clean_filename(p_data['displayName'])
+            fname = clean_filename(value['displayName'])
             ## Save Apple Enrollment Pofile as JSON or YAML depending on configured value in "-o"
             if output != "json":
                 with open(configpath+fname+".yaml", 'w') as yamlFile:
-                    yaml.dump(p_data, yamlFile, sort_keys=False,
-                              default_flow_style=False)
+                    yaml.dump(value, yamlFile, sort_keys=False,
+                            default_flow_style=False)
             else:
                 with open(configpath+fname+".json", 'w') as jsonFile:
-                    json.dump(p_data, jsonFile, indent=10)
+                    json.dump(value, jsonFile, indent=10)
