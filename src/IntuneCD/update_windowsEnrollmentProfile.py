6ed0a4762736ae209d341e505fb8b0cc9c401053
Tobias Almén <almenscorner@outlook.com>
2022-03-04 11:42:00 +0100

    Batch assignments, match displayName and use new assignments module

--- src/IntuneCD/update_windowsEnrollmentProfile.py
+++ src/IntuneCD/update_windowsEnrollmentProfile.py
@@ -17,7 +17,8 @@ import yaml
 import re
 
 from .graph_request import makeapirequest, makeapirequestPatch, makeapirequestPost
-from .get_add_assignments import add_assignment
+from .graph_batch import batch_assignment, get_object_assignment
+from .update_assignment import update_assignment, post_assignment_update
 from deepdiff import DeepDiff
 
 ## Set MS Graph endpoint
@@ -30,6 +31,11 @@ def update(path, token, assignment=False):
     configpath = path+"/"+"Enrollment Profiles/Windows"
     ## If Windows Enrollment Profile path exists, continue
     if os.path.exists(configpath) == True:
+        ## Get enrollment profiles
+        mem_data = makeapirequest(endpoint, token)
+        ## Get current assignment
+        mem_assignments = batch_assignment(mem_data,'deviceManagement/windowsAutopilotDeploymentProfiles/','/assignments',token)
+
         for filename in os.listdir(configpath):
             file = os.path.join(configpath, filename)
             # If path is Directory, skip
@@ -44,36 +50,33 @@ def update(path, token, assignment=False):
                     if filename.endswith(".yaml"):
                         data = json.dumps(yaml.safe_load(f))
                         repo_data = json.loads(data)
-                        q_param = {"$filter": "displayName eq " +
-                            "'" + repo_data['displayName'] + "'"}
+                        
                     elif filename.endswith(".json"):
                         f = open(file)
                         repo_data = json.load(f)
-                        q_param = {"$filter": "displayName eq " + "'" + repo_data['displayName'] + "'"}
 
                     ## Create object to pass in to assignment function
                     assign_obj = {}
                     if "assignments" in repo_data:
-                        assign_obj['assignments'] = repo_data['assignments']
+                        assign_obj = repo_data['assignments']
                     repo_data.pop('assignments', None)
 
-                    ## Get Windows Enrollment Profile with query parameter
-                    mem_data = makeapirequest(endpoint, token,q_param)
+                    data = {'value':''}
+                    if mem_data['value']:
+                        for val in mem_data['value']:
+                            if repo_data['displayName'] == val['displayName']:
+                                data['value'] = val
 
                     ## If Windows Enrollment Profile exists, continue
-                    if mem_data['value']:
+                    if data['value']:
                         print("-" * 90)
-                        pid = mem_data['value'][0]['id']
+                        mem_id = data['value']['id']
                         ## Remove keys before using DeepDiff
                         remove_keys = {'id', 'createdDateTime','version','lastModifiedDateTime'}
                         for k in remove_keys:
                             mem_data['value'][0].pop(k, None)
 
-                        ## Check if assignment needs updating and apply chanages
-                        if assignment == True:
-                            add_assignment(endpoint, assign_obj,pid,token,status_code=201,extra_url=None,wap=True)
-
-                        diff = DeepDiff(mem_data['value'][0], repo_data, ignore_order=True).get('values_changed', {})
+                        diff = DeepDiff(data['value'], repo_data, ignore_order=True).get('values_changed', {})
 
                         ## If any changed values are found, push them to Intune
                         if diff:
@@ -90,10 +93,18 @@ def update(path, token, assignment=False):
                             else:
                                 repo_data['managementServiceAppId'] = ""
                             request_data = json.dumps(repo_data)
-                            makeapirequestPatch(endpoint + "/" + pid, token,q_param,request_data)
+                            makeapirequestPatch(endpoint + "/" + mem_id, token,q_param,request_data)
                         else:
                             print(
                                 'No difference found for Windows Enrollment profile: ' + repo_data['displayName'])
+
+                        if assignment == True:
+                            mem_assign_obj = get_object_assignment(mem_id,mem_assignments)
+                            update = update_assignment(assign_obj,mem_assign_obj,token)
+                            if update is not None:
+                                request_data = {}
+                                request_data['target'] = update
+                                post_assignment_update(request_data,mem_id,'deviceManagement/windowsAutopilotDeploymentProfiles','assign',token,status_code=201)
 
                     ## If Autopilot profile does not exist, create it and assign
                     else:
@@ -102,5 +113,10 @@ def update(path, token, assignment=False):
                             "Autopilot profile not found, creating profile: " + repo_data['displayName'])
                         request_json = json.dumps(repo_data)
                         post_request = makeapirequestPost(endpoint, token,q_param=None,jdata=request_json,status_code=201)
-                        add_assignment(endpoint, assign_obj,post_request['id'],token,status_code=201,extra_url=None,wap=True)
+                        mem_assign_obj = []
+                        assignment = update_assignment(assign_obj,mem_assign_obj,token)
+                        if assignment is not None:
+                            request_data = {}
+                            request_data['target'] = assignment
+                            post_assignment_update(request_data,post_request['id'],'deviceManagement/windowsAutopilotDeploymentProfiles','assign',token,status_code=201)
                         print("Autopilot profile created with id: " + post_request['id'])
