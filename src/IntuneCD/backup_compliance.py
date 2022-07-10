ef3d66849c5c50e67e6020ffcb0eae4c65695733
Tobias Almén <almenscorner@outlook.com>
2022-07-11 00:17:51 +0200

    Adding count, improved documentation and code readability

--- src/IntuneCD/backup_compliance.py
+++ src/IntuneCD/backup_compliance.py
@@ -2,65 +2,59 @@
 
 """
 This module backs up all Compliance Polices in Intune.
-
-Parameters
-----------
-path : str
-    The path to save the backup to
-output : str
-    The format the backup will be saved as
-token : str
-    The token to use for authenticating the request
 """
-
-import json
-import os
-import yaml
 
 from .clean_filename import clean_filename
 from .graph_request import makeapirequest
 from .graph_batch import batch_assignment, get_object_assignment
+from .save_output import save_output
+from .remove_keys import remove_keys
 
-## Set MS Graph endpoint
-endpoint = "https://graph.microsoft.com/beta/deviceManagement/deviceCompliancePolicies"
+# Set MS Graph endpoint
+ENDPOINT = "https://graph.microsoft.com/beta/deviceManagement/deviceCompliancePolicies"
 
-## Get all Compliance policies and save them in specified path
+
+# Get all Compliance policies and save them in specified path
 def savebackup(path, output, exclude, token):
-    configpath = path+"/"+"Compliance Policies/Policies/"
+    """
+    Saves all Compliance policies in Intune to a JSON or YAML file.
+
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param exclude: If "assignments" is in the list, it will not back up the assignments
+    :param token: Token to use for authenticating the request
+    """
+
+    config_count = 0
+    configpath = path + "/" + "Compliance Policies/Policies/"
     q_param = {
         "$expand": "scheduledActionsForRule($expand=scheduledActionConfigurations)"}
-    data = makeapirequest(endpoint, token, q_param)
+    data = makeapirequest(ENDPOINT, token, q_param)
 
-    assignment_responses = batch_assignment(data,f'deviceManagement/deviceCompliancePolicies/','/assignments',token)
+    assignment_responses = batch_assignment(
+        data, 'deviceManagement/deviceCompliancePolicies/', '/assignments', token)
 
     for policy in data['value']:
+        config_count += 1
         print("Backing up compliance policy: " + policy['displayName'])
 
         if "assignments" not in exclude:
-            assignments = get_object_assignment(policy['id'],assignment_responses)
+            assignments = get_object_assignment(
+                policy['id'], assignment_responses)
             if assignments:
                 policy['assignments'] = assignments
 
-        remove_keys = {'id', 'createdDateTime', 'version', 'lastModifiedDateTime', '@odata.context',
-                       'scheduledActionConfigurations@odata.context', 'scheduledActionsForRule@odata.context'}
-        for k in remove_keys:
-            policy.pop(k, None)
+            policy = remove_keys(policy)
             for rule in policy['scheduledActionsForRule']:
-                rule.pop(k, None)
+                remove_keys(rule)
             if policy['scheduledActionsForRule']:
                 for scheduled_config in policy['scheduledActionsForRule'][0]['scheduledActionConfigurations']:
-                    scheduled_config.pop(k, None)
+                    remove_keys(scheduled_config)
 
-        if os.path.exists(configpath) == False:
-            os.makedirs(configpath)
-
-        ## Get filename without illegal characters
+        # Get filename without illegal characters
         fname = clean_filename(policy['displayName'])
-        ## Save Compliance policy as JSON or YAML depending on configured value in "-o"
-        if output != "json":
-            with open(configpath+fname+'_'+str(policy['@odata.type'].split('.')[2])+".yaml", 'w') as yamlFile:
-                yaml.dump(policy, yamlFile, sort_keys=False,
-                          default_flow_style=False)
-        else:
-            with open(configpath+fname+'_'+str(policy['@odata.type'].split('.')[2])+".json", 'w') as jsonFile:
-                json.dump(policy, jsonFile, indent=10)
+        # Save Compliance policy as JSON or YAML depending on configured value
+        # in "-o"
+        save_output(output, configpath, f'{fname}_' + str(policy['@odata.type'].split('.')[2]), policy)
+
+    return config_count
