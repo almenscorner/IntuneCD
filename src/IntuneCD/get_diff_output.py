12bbcf079758e8a81f9215e6286140c8896fff7f
Tobias Almén <almenscorner@outlook.com>
2022-07-11 00:20:36 +0200

    Adding function to get diff output as string

---
+++ src/IntuneCD/get_diff_output.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+This module is used to get diff output from the diff.
+"""
+
+import re
+
+
+def get_diff_output(data):
+    """
+    This function takes the data from the diff and returns a string with the differences.
+
+    :param data: The data from the diff
+    :return: A string with the differences
+    """
+
+    for key, value in data.items():
+        setting = re.search(
+            "\\[(.*)\\]", key).group(1).split("[")[-1]
+        new_val = value['new_value']
+        old_val = value['old_value']
+
+    return f"Setting: {setting}, New Value: {new_val}, Old Value: {old_val}"
