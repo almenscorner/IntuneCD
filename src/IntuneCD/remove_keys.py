0cbed70c37500064bb9b0aec9d2752847540383d
Tobias Almén <almenscorner@outlook.com>
2022-07-11 00:23:07 +0200

    Adding function to remove unwanted keys

---
+++ src/IntuneCD/remove_keys.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+"""
+This module is used to remove keys from the data.
+"""
+
+
+def remove_keys(data):
+    """
+    This function removes keys from the data.
+    :param data: The data to remove keys from.
+    :return: The data with removed keys.
+    """
+
+    keys = {
+        'id',
+        'version',
+        'topicIdentifier',
+        'certificate',
+        'createdDateTime',
+        'lastModifiedDateTime',
+        'isDefault'
+        'deployedAppCount',
+        'isAssigned',
+        '@odata.context',
+        'scheduledActionConfigurations@odata.context',
+        'scheduledActionsForRule@odata.context',
+        'sourceId',
+        'supportsScopeTags',
+        'companyCodes',
+        'isGlobalScript',
+        'highestAvailableVersion',
+        'token',
+        'lastSyncDateTime',
+        'isReadOnly',
+        'secretReferenceValueId',
+        'isEncrypted'}
+    for k in keys:
+        data.pop(k, None)
+
+    return data
