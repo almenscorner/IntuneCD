ec6bd2b93fff1f837e27a09f4f10de954391c4f6
Tobias Almén <almenscorner@outlook.com>
2022-07-11 00:17:28 +0200

    Adding count, improved documentation and code readability

--- src/IntuneCD/backup_appConfiguration.py
+++ src/IntuneCD/backup_appConfiguration.py
@@ -2,51 +2,51 @@
 
 """
 This module backs up all App Configuration Polices in Intune.
-
-Parameters
-----------
-path : str
-    The path to save the backup to
-output : str
-    The format the backup will be saved as
-token : str
-    The token to use for authenticating the request
 """
-
-import json
-import os
-import yaml
 
 from .clean_filename import clean_filename
 from .graph_request import makeapirequest
 from .graph_batch import batch_assignment, get_object_assignment
+from .save_output import save_output
+from .remove_keys import remove_keys
 
-## Set MS Graph endpoint
-endpoint = "https://graph.microsoft.com/beta/deviceAppManagement/mobileAppConfigurations"
-app_endpoint = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps"
+# Set MS Graph endpoint
+ENDPOINT = "https://graph.microsoft.com/beta/deviceAppManagement/mobileAppConfigurations"
+APP_ENDPOINT = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps"
 
-## Get all App Configuration policies and save them in specified path
+
+# Get all App Configuration policies and save them in specified path
 def savebackup(path, output, exclude, token):
-    configpath = path+"/"+"App Configuration/"
-    data = makeapirequest(endpoint, token)
+    """
+    Saves all App Configuration policies in Intune to a JSON or YAML file.
 
-    assignment_responses = batch_assignment(data,f'deviceAppManagement/mobileAppConfigurations/','/assignments',token)
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param exclude: If "assignments" is in the list, it will not back up the assignments
+    :param token: Token to use for authenticating the request
+    """
+
+    config_count = 0
+    configpath = path + "/" + "App Configuration/"
+    data = makeapirequest(ENDPOINT, token)
+
+    assignment_responses = batch_assignment(
+        data, 'deviceAppManagement/mobileAppConfigurations/', '/assignments', token)
 
     for profile in data['value']:
+        config_count += 1
         if "assignments" not in exclude:
-            assignments = get_object_assignment(profile['id'],assignment_responses)
+            assignments = get_object_assignment(
+                profile['id'], assignment_responses)
             if assignments:
                 profile['assignments'] = assignments
-            
-        remove_keys = {'id', 'createdDateTime',
-                       'version', 'lastModifiedDateTime'}
-        for k in remove_keys:
-            profile.pop(k, None)
 
-        ## Get name and type of app on App Configuration Profile
+        profile = remove_keys(profile)
+
+        # Get name and type of app on App Configuration Profile
         app_dict = {}
         for app_id in profile['targetedMobileApps']:
-            app_data = makeapirequest(app_endpoint + "/" + app_id, token)
+            app_data = makeapirequest(APP_ENDPOINT + "/" + app_id, token)
             if app_data:
                 app_dict['appName'] = app_data['displayName']
                 app_dict['type'] = app_data['@odata.type']
@@ -57,16 +57,11 @@ def savebackup(path, output, exclude, token):
 
         print("Backing up App Configuration: " + profile['displayName'])
 
-        if os.path.exists(configpath) == False:
-            os.mkdir(configpath)
+        # Get filename without illegal characters
+        fname = clean_filename(
+            f"{profile['displayName']}_{str(profile['@odata.type'].split('.')[2])}")
+        # Save App Configuration as JSON or YAML depending on configured value
+        # in "-o"
+        save_output(output, configpath, fname, profile)
 
-        ## Get filename without illegal characters
-        fname = clean_filename(f"{profile['displayName']}_{str(profile['@odata.type'].split('.')[2])}")
-        ## Save App Condiguration as JSON or YAML depending on configured value in "-o"
-        if output != "json":
-            with open(configpath+fname+".yaml", 'w') as yamlFile:
-                yaml.dump(profile, yamlFile, sort_keys=False,
-                          default_flow_style=False)
-        else:
-            with open(configpath+fname+".json", 'w') as jsonFile:
-                json.dump(profile, jsonFile, indent=10)
+    return config_count
