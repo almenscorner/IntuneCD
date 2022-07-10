39fffbeef24b506d7802b6abc7fff2b8c21fca5f
Tobias Almén <almenscorner@outlook.com>
2022-07-11 00:18:49 +0200

    Adding count, improved documentation and code readability

--- src/IntuneCD/backup_windowsEnrollmentProfile.py
+++ src/IntuneCD/backup_windowsEnrollmentProfile.py
@@ -2,57 +2,56 @@
 
 """
 This module backs up all Windows Enrollment Profiles in Intune.
-
-Parameters
-----------
-path : str
-    The path to save the backup to
-output : str
-    The format the backup will be saved as
-token : str
-    The token to use for authenticating the request
 """
-
-import json
-import os
-import yaml
 
 from .clean_filename import clean_filename
 from .graph_request import makeapirequest
-from .graph_batch import batch_assignment,get_object_assignment
+from .graph_batch import batch_assignment, get_object_assignment
+from .save_output import save_output
+from .remove_keys import remove_keys
 
-## Set MS Graph endpoint
-endpoint = "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeploymentProfiles"
+# Set MS Graph endpoint
+ENDPOINT = "https://graph.microsoft.com/beta/deviceManagement/windowsAutopilotDeploymentProfiles"
 
-## Get all Windows Enrollment Profiles and save them in specified path
+
+# Get all Windows Enrollment Profiles and save them in specified path
 def savebackup(path, output, exclude, token):
-    configpath = path+"/"+"Enrollment Profiles/Windows/"
-    data = makeapirequest(endpoint, token)
+    """
+    Saves all Windows Enrollment Profiles in Intune to a JSON or YAML file.
 
-    assignment_responses = batch_assignment(data,'deviceManagement/windowsAutopilotDeploymentProfiles/','/assignments',token)
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param exclude: If "assignments" is in the list, it will not back up the assignments
+    :param token: Token to use for authenticating the request
+    """
+
+    config_count = 0
+    configpath = path + "/" + "Enrollment Profiles/Windows/"
+    data = makeapirequest(ENDPOINT, token)
+
+    assignment_responses = batch_assignment(
+        data,
+        'deviceManagement/windowsAutopilotDeploymentProfiles/',
+        '/assignments',
+        token)
 
     for profile in data['value']:
+        config_count += 1
         if "assignments" not in exclude:
-            assignments = get_object_assignment(profile['id'],assignment_responses)
+            assignments = get_object_assignment(
+                profile['id'], assignment_responses)
             if assignments:
                 profile['assignments'] = assignments
-        
-        remove_keys = {'id', 'createdDateTime',
-                       'version', 'lastModifiedDateTime'}
-        for k in remove_keys:
-            profile.pop(k, None)
+
+        profile = remove_keys(profile)
+
         print("Backing up Autopilot enrollment profile: " +
               profile['displayName'])
-        if os.path.exists(configpath) == False:
-            os.makedirs(configpath)
 
-        ## Get filename without illegal characters
+        # Get filename without illegal characters
         fname = clean_filename(profile['displayName'])
-        ## Save Windows Enrollment Profile as JSON or YAML depending on configured value in "-o"
-        if output != "json":
-            with open(configpath+fname+".yaml", 'w') as yamlFile:
-                yaml.dump(profile, yamlFile, sort_keys=False,
-                          default_flow_style=False)
-        else:
-            with open(configpath+fname+".json", 'w') as jsonFile:
-                json.dump(profile, jsonFile, indent=10)
+        # Save Windows Enrollment Profile as JSON or YAML depending on
+        # configured value in "-o"
+        save_output(output, configpath, fname, profile)
+
+    return config_count
