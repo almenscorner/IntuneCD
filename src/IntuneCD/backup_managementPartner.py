0c09d9c8c93750151e89ac1ebeb358edc5ddcd6e
Tobias Almén <almenscorner@outlook.com>
2022-07-11 00:18:18 +0200

    Adding count, improved documentation and code readability

--- src/IntuneCD/backup_managementPartner.py
+++ src/IntuneCD/backup_managementPartner.py
@@ -2,52 +2,44 @@
 
 """
 This module backs up all Management Partners in Intune.
-
-Parameters
-----------
-path : str
-    The path to save the backup to
-output : str
-    The format the backup will be saved as
-token : str
-    The token to use for authenticating the request
 """
-
-import json
-import os
-import yaml
 
 from .clean_filename import clean_filename
 from .graph_request import makeapirequest
+from .save_output import save_output
+from .remove_keys import remove_keys
 
-## Set MS Graph endpoint
-endpoint = "https://graph.microsoft.com/beta/deviceManagement/deviceManagementPartners"
+# Set MS Graph endpoint
+ENDPOINT = "https://graph.microsoft.com/beta/deviceManagement/deviceManagementPartners"
 
-## Get all Management Partners and save them in specified path
+
+# Get all Management Partners and save them in specified path
 def savebackup(path, output, token):
-    configpath = path+"/"+"Partner Connections/Management/"
-    data = makeapirequest(endpoint, token)
+    """
+    Saves Management Partner information in Intune to a JSON or YAML file.
+
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param token: Token to use for authenticating the request
+    """
+
+    config_count = 0
+    configpath = path + "/" + "Partner Connections/Management/"
+    data = makeapirequest(ENDPOINT, token)
 
     for partner in data['value']:
+        config_count += 1
         if partner['isConfigured'] is False:
             continue
         else:
             print("Backing up Management Partner: " + partner['displayName'])
-            
-            remove_keys = {'id', 'createdDateTime', 'version', 'lastModifiedDateTime'}
-            for k in remove_keys:
-                partner.pop(k, None)
 
-            if os.path.exists(configpath) == False:
-                os.makedirs(configpath)
+            partner = remove_keys(partner)
 
-            ## Get filename without illegal characters
+            # Get filename without illegal characters
             fname = clean_filename(partner['displayName'])
-            ## Save Compliance policy as JSON or YAML depending on configured value in "-o"
-            if output != "json":
-                with open(configpath+fname+".yaml", 'w') as yamlFile:
-                    yaml.dump(partner, yamlFile, sort_keys=False,
-                            default_flow_style=False)
-            else:
-                with open(configpath+fname+".json", 'w') as jsonFile:
-                    json.dump(partner, jsonFile, indent=10)
+            # Save Compliance policy as JSON or YAML depending on configured
+            # value in "-o"
+            save_output(output, configpath, fname, partner)
+
+    return config_count
