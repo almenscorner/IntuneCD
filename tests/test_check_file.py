b069b2ddd09bb3c246b7cd3aaef56c880f2b55f2
Tobias Almén <almenscorner@outlook.com>
2022-07-19 13:47:43 +0200

    If name is main check

--- tests/test_check_file.py
+++ tests/test_check_file.py
@@ -22,7 +22,8 @@ class TestCheckFile(unittest.TestCase):
         self.DS_Store = ".DS_Store"
         self.d = self.directory.makedir("fake_path")
 
-        self.yaml_file = self.directory.write(self.yaml, "yaml", encoding="utf-8")
+        self.yaml_file = self.directory.write(
+            self.yaml, "yaml", encoding="utf-8")
         self.json_file = self.directory.write(
             self.json, '{"data": "fake_data"}', encoding="utf-8")
         self.md_file = self.directory.write(self.md, "md", encoding="utf-8")
@@ -33,21 +34,29 @@ class TestCheckFile(unittest.TestCase):
         self.directory.cleanup()
 
     def test_check_yaml_file(self):
+        """Should return True if the file is a yaml file."""
         self.check = check_file(self.directory.path, self.yaml)
         self.assertTrue(self.check)
 
     def test_check_json_file(self):
+        """Should return True if the file is a json file."""
         self.check = check_file(self.directory.path, self.json)
         self.assertTrue(self.check)
 
     def test_check_md_file(self):
+        """Should return False if the file is a md file."""
         self.check = check_file(self.directory.path, self.md)
         self.assertFalse(self.check)
 
     def test_check_DS_Store_file(self):
+        """Should return False if the file is a DS_Store file."""
         self.check = check_file(self.directory.path, self.DS_Store)
         self.assertFalse(self.check)
 
     def test_check_dir(self):
+        """Should return False if the file is a directory."""
         self.check = check_file(self.directory.path, self.d)
         self.assertFalse(self.check)
+
+if __name__ == '__main__':
+    unittest.main()
