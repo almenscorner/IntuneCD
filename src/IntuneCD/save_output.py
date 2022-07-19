8b80d69371eeef3de532b6fa647d7f9e19680ee3
Tobias Almén <almenscorner@outlook.com>
2022-07-19 02:01:01 +0200

    Update save_output.py

--- src/IntuneCD/save_output.py
+++ src/IntuneCD/save_output.py
@@ -22,10 +22,13 @@ def save_output(output, configpath, fname, data):
     if not os.path.exists(configpath):
         os.makedirs(configpath)
 
-    if output != "json":
+    if output == 'yaml':
         with open(configpath + fname + ".yaml", 'w') as yamlFile:
             yaml.dump(data, yamlFile, sort_keys=False,
                       default_flow_style=False)
-    else:
+    elif output == 'json':
         with open(configpath + fname + ".json", 'w') as jsonFile:
             json.dump(data, jsonFile, indent=10)
+
+    else:
+        raise ValueError("Invalid output format")
