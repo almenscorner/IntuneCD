f0b318de475be82c130cb6f2532fe4db4ee619af
Tobias Almén <almenscorner@outlook.com>
2022-07-19 02:01:38 +0200

    Update update_frontend.py

--- src/IntuneCD/update_frontend.py
+++ src/IntuneCD/update_frontend.py
@@ -7,9 +7,6 @@ This module is used to update the IntuneCD frontend.
 import os
 import requests
 
-API_KEY = os.environ.get("API_KEY")
-
-
 def update_frontend(frontend, data):
     """
     This function updates the frontend with the number of configurations, diff count backup stream and update stream.
@@ -17,6 +14,8 @@ def update_frontend(frontend, data):
     :param frontend: The frontend to update
     :param data: The data to update the frontend with
     """
+    API_KEY = os.environ.get("API_KEY")
+
     if not API_KEY:
         raise Exception("API_KEY environment variable is not set")
 
@@ -25,5 +24,4 @@ def update_frontend(frontend, data):
         response = requests.post(frontend, json=data, headers=headers)
 
         if response.status_code != 200:
-            print("Error updating frontend")
-            print(response.text)
+            raise Exception(f"Error updating frontend, {response.text}")
