20b4a9125cf577bd9eb1a022afcbbbad26f29d78
Tobias Almén <almenscorner@outlook.com>
2022-11-14 08:15:39 +0100

    Update test_documentation_functions.py

--- tests/test_documentation_functions.py
+++ tests/test_documentation_functions.py
@@ -106,7 +106,7 @@ class TestDocumentationFunctions(unittest.TestCase):
             "config/test_file_name.json",
             '{"test": "test", "name": "test", "description": "test", "testvals": "1,2", "testlist": ["test"], "testdict": [{"test": "test"}], "assignments": [{"intent": "apply", "target": {"@odata.type": "#test", "groupName": "test-group", "deviceAndAppManagementAssignmentFilterId": "test-filter", "deviceAndAppManagementAssignmentFilterType": "test"}}]}',
             encoding="utf-8")
-        self.expected_data = '#test##testDescription:test###Assignments|intent|target|filtertype|filtername||------|----------|-----------|-----------||apply|test-group|test|test-filter||setting|value||--------|------------||Test|test||Name|test||Testvals|**1<br/>**2|'
+        self.expected_data = '#test##testDescription:test###Assignments|intent|target|filtertype|filtername||------|----------|-----------|-----------||apply|test-group|test|test-filter||setting|value||--------|------------||Test|test||Name|test||Testvals|**1<br/>**2||Testlist|||Testdict|test:test|'
 
         document_configs(
             f"{self.directory.path}/config",
