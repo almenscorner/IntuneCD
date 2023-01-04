f5a17f9824fc7bf77b3ec528624c189bbdd17ac1
Tobias Almén <almenscorner@outlook.com>
2023-01-04 09:00:26 +0100

    Black code style

--- tests/Backup/test_backup_assignmentFilter.py
+++ tests/Backup/test_backup_assignmentFilter.py
@@ -18,30 +18,30 @@ ASSIGNMENT_FILTER = {
             "description": "",
             "id": "0",
             "platform": "macOS",
-            "rule": "(device.model -eq \"macbook Pro\")",
-            "roleScopeTags": ["0"]
-        }]}
+            "rule": '(device.model -eq "macbook Pro")',
+            "roleScopeTags": ["0"],
+        }
+    ]
+}
 
 
 @patch("src.IntuneCD.backup_assignmentFilters.savebackup")
-@patch("src.IntuneCD.backup_assignmentFilters.makeapirequest",
-       return_value=ASSIGNMENT_FILTER)
+@patch("src.IntuneCD.backup_assignmentFilters.makeapirequest", return_value=ASSIGNMENT_FILTER)
 class TestBackupAssignmentFilters(unittest.TestCase):
     """Test class for backup_assignmentFilters."""
 
     def setUp(self):
         self.directory = TempDirectory()
         self.directory.create()
-        self.token = 'token'
+        self.token = "token"
         self.saved_path = f"{self.directory.path}/Filters/macOS - Model."
         self.expected_data = {
             "displayName": "macOS - Model",
             "description": "",
             "platform": "macOS",
-            "rule": "(device.model -eq \"macbook Pro\")",
-            "roleScopeTags": [
-                "0"
-            ]}
+            "rule": '(device.model -eq "macbook Pro")',
+            "roleScopeTags": ["0"],
+        }
 
     def tearDown(self):
         self.directory.cleanup()
@@ -49,35 +49,35 @@ class TestBackupAssignmentFilters(unittest.TestCase):
     def test_backup_yml(self, mock_data, mock_makeapirequest):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
 
-        self.count = savebackup(self.directory.path, 'yaml', self.token)
+        self.count = savebackup(self.directory.path, "yaml", self.token)
 
-        with open(self.saved_path + 'yaml', 'r') as f:
+        with open(self.saved_path + "yaml", "r") as f:
             data = json.dumps(yaml.safe_load(f))
             self.saved_data = json.loads(data)
 
-        self.assertTrue(Path(f'{self.directory.path}/Filters').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Filters").exists())
         self.assertEqual(self.expected_data, self.saved_data)
         self.assertEqual(1, self.count)
 
     def test_backup_json(self, mock_data, mock_makeapirequest):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
 
-        self.count = savebackup(self.directory.path, 'json', self.token)
+        self.count = savebackup(self.directory.path, "json", self.token)
 
-        with open(self.saved_path + 'json', 'r') as f:
+        with open(self.saved_path + "json", "r") as f:
             self.saved_data = json.load(f)
 
-        self.assertTrue(Path(f'{self.directory.path}/Filters').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Filters").exists())
         self.assertEqual(self.expected_data, self.saved_data)
         self.assertEqual(1, self.count)
 
     def test_backup_with_no_return_data(self, mock_data, mock_makeapirequest):
         """The count should be 0 if no data is returned."""
 
-        mock_data.return_value = {'value': []}
-        self.count = savebackup(self.directory.path, 'json', self.token)
+        mock_data.return_value = {"value": []}
+        self.count = savebackup(self.directory.path, "json", self.token)
         self.assertEqual(0, self.count)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
