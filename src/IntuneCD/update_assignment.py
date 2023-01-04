c6c70fd0f59e96518fd9a6f3cd3d33ccbabbd50b
Tobias Almén <almenscorner@outlook.com>
2023-01-04 08:58:27 +0100

    Black code style

--- src/IntuneCD/update_assignment.py
+++ src/IntuneCD/update_assignment.py
@@ -19,31 +19,30 @@ def get_added_removed(diff_object) -> list:
 
     update = []
     for root in diff_object:
-        intent = ''
-        filterID = ''
-        filterType = ''
-        target = ''
+        intent = ""
+        filterID = ""
+        filterType = ""
+        target = ""
 
-        if 'intent' in diff_object[root]:
-            intent = diff_object[root]['intent']
+        if "intent" in diff_object[root]:
+            intent = diff_object[root]["intent"]
 
-        if 'target' in diff_object[root]:
-            if 'deviceAndAppManagementAssignmentFilterId' in diff_object[root]['target']:
-                filterID = diff_object[root]['target']['deviceAndAppManagementAssignmentFilterId']
-            if 'deviceAndAppManagementAssignmentFilterType' in diff_object[root]['target']:
-                filterType = diff_object[root]['target']['deviceAndAppManagementAssignmentFilterType']
+        if "target" in diff_object[root]:
+            if "deviceAndAppManagementAssignmentFilterId" in diff_object[root]["target"]:
+                filterID = diff_object[root]["target"]["deviceAndAppManagementAssignmentFilterId"]
+            if "deviceAndAppManagementAssignmentFilterType" in diff_object[root]["target"]:
+                filterType = diff_object[root]["target"]["deviceAndAppManagementAssignmentFilterType"]
 
-            if diff_object[root]['target']['@odata.type'] == '#microsoft.graph.groupAssignmentTarget':
-                target = diff_object[root]['target']['groupId']
+            if diff_object[root]["target"]["@odata.type"] == "#microsoft.graph.groupAssignmentTarget":
+                target = diff_object[root]["target"]["groupId"]
 
-            if diff_object[root]['target']['@odata.type'] == '#microsoft.graph.allDevicesAssignmentTarget':
-                target = 'All Devices'
+            if diff_object[root]["target"]["@odata.type"] == "#microsoft.graph.allDevicesAssignmentTarget":
+                target = "All Devices"
 
-            if diff_object[root]['target']['@odata.type'] == '#microsoft.graph.allLicensedUsersAssignmentTarget':
-                target = 'All Users'
+            if diff_object[root]["target"]["@odata.type"] == "#microsoft.graph.allLicensedUsersAssignmentTarget":
+                target = "All Users"
 
-        update.append(
-            f"intent: {intent}, Filter ID: {filterID}, Filter Type: {filterType}, target: {target}")
+        update.append(f"intent: {intent}, Filter ID: {filterID}, Filter Type: {filterType}, target: {target}")
 
     return update
 
@@ -59,44 +58,45 @@ def update_assignment(repo, mem, token) -> list:
     """
 
     diff = DeepDiff(mem, repo, ignore_order=True)
-    added = diff.get('iterable_item_added', {})
+    added = diff.get("iterable_item_added", {})
     update = False
     if diff:
         for val in repo:
             # Request group id based on group name
-            if 'groupName' in val['target']:
+            if "groupName" in val["target"]:
                 request = makeapirequest(
-                    "https://graph.microsoft.com/beta/groups", token, {
-                        "$filter": "displayName eq " + "'" + val['target']['groupName'] + "'"})
-                if request['value']:
-                    val['target'].pop('groupName')
-                    val['target']['groupId'] = request['value'][0]['id']
+                    "https://graph.microsoft.com/beta/groups",
+                    token,
+                    {"$filter": "displayName eq " + "'" + val["target"]["groupName"] + "'"},
+                )
+                if request["value"]:
+                    val["target"].pop("groupName")
+                    val["target"]["groupId"] = request["value"][0]["id"]
 
             # Request filter id based on filter name
-            if val['target']['deviceAndAppManagementAssignmentFilterId']:
-                filters = makeapirequest(
-                    "https://graph.microsoft.com/beta/deviceManagement/assignmentFilters", token)
-                for filter in filters['value']:
-                    if val['target']['deviceAndAppManagementAssignmentFilterId'] == filter['displayName']:
-                        val['target']['deviceAndAppManagementAssignmentFilterId'] = filter['id']
+            if val["target"]["deviceAndAppManagementAssignmentFilterId"]:
+                filters = makeapirequest("https://graph.microsoft.com/beta/deviceManagement/assignmentFilters", token)
+                for filter in filters["value"]:
+                    if val["target"]["deviceAndAppManagementAssignmentFilterId"] == filter["displayName"]:
+                        val["target"]["deviceAndAppManagementAssignmentFilterId"] = filter["id"]
 
                 # If filter is None, remove keys
-                if val['target']['deviceAndAppManagementAssignmentFilterId'] is None:
-                    val['target'].pop(
-                        'deviceAndAppManagementAssignmentFilterId')
-                    val['target'].pop(
-                        'deviceAndAppManagementAssignmentFilterType')
+                if val["target"]["deviceAndAppManagementAssignmentFilterId"] is None:
+                    val["target"].pop("deviceAndAppManagementAssignmentFilterId")
+                    val["target"].pop("deviceAndAppManagementAssignmentFilterType")
 
         for val in repo:
-            if 'groupId' in val['target'] or '#microsoft.graph.allDevicesAssignmentTarget' in val['target'][
-                '@odata.type'] \
-                    or '#microsoft.graph.allLicensedUsersAssignmentTarget' in val['target']['@odata.type']:
+            if (
+                "groupId" in val["target"]
+                or "#microsoft.graph.allDevicesAssignmentTarget" in val["target"]["@odata.type"]
+                or "#microsoft.graph.allLicensedUsersAssignmentTarget" in val["target"]["@odata.type"]
+            ):
                 update = True
 
         if update is True:
             # Print added assignments
             if added:
-                print('Updating assignments, added assignments:')
+                print("Updating assignments, added assignments:")
                 updates = get_added_removed(added)
                 for update in updates:
                     print(update)
@@ -119,10 +119,5 @@ def post_assignment_update(object, id, url, extra_url, token, status_code=200):
     """
 
     request_json = json.dumps(object)
-    url = f'https://graph.microsoft.com/beta/{url}/{id}/{extra_url}'
-    makeapirequestPost(
-        url,
-        token,
-        q_param=None,
-        jdata=request_json,
-        status_code=status_code)
+    url = f"https://graph.microsoft.com/beta/{url}/{id}/{extra_url}"
+    makeapirequestPost(url, token, q_param=None, jdata=request_json, status_code=status_code)
