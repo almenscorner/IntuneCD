456ec197ead07c79370c57b363005dbcb2fc1cbf
Tobias Almén <almenscorner@outlook.com>
2023-01-04 09:02:01 +0100

    Black code style

--- tests/test_match.py
+++ tests/test_match.py
@@ -14,44 +14,44 @@ class TestMatch(unittest.TestCase):
 
     def test_match_macOS(self):
         """The odata type should match the expected value."""
-        self.app = {'@odata.type': '#microsoft.graph.macOsVppApp'}
+        self.app = {"@odata.type": "#microsoft.graph.macOsVppApp"}
 
-        self.result = ('macos', str(self.app['@odata.type']).lower())
+        self.result = ("macos", str(self.app["@odata.type"]).lower())
 
         self.assertTrue(self.result)
 
     def test_match_ios(self):
         """The odata type should match the expected value."""
-        self.app = {'@odata.type': '#microsoft.graph.iosStoreApp'}
+        self.app = {"@odata.type": "#microsoft.graph.iosStoreApp"}
 
-        self.result = ('ios', str(self.app['@odata.type']).lower())
+        self.result = ("ios", str(self.app["@odata.type"]).lower())
 
         self.assertTrue(self.result)
 
     def test_match_windows(self):
         """The odata type should match the expected value."""
-        self.app = {'@odata.type': '#microsoft.graph.win32LobApp'}
+        self.app = {"@odata.type": "#microsoft.graph.win32LobApp"}
 
-        self.result = match('win32', str(self.app['@odata.type']).lower())
+        self.result = match("win32", str(self.app["@odata.type"]).lower())
 
         self.assertTrue(self.result)
 
     def test_match_android(self):
         """The odata type should match the expected value."""
-        self.app = {'@odata.type': '#microsoft.graph.androidStoreApp'}
+        self.app = {"@odata.type": "#microsoft.graph.androidStoreApp"}
 
-        self.result = match('android', str(self.app['@odata.type']).lower())
+        self.result = match("android", str(self.app["@odata.type"]).lower())
 
         self.assertTrue(self.result)
 
     def test_match_unknown(self):
         """Unkown odata type should return false."""
-        self.app = {'@odata.type': '#microsoft.graph.unknown'}
+        self.app = {"@odata.type": "#microsoft.graph.unknown"}
 
-        self.result = match('iOS', str(self.app['@odata.type']).lower())
+        self.result = match("iOS", str(self.app["@odata.type"]).lower())
 
         self.assertFalse(self.result)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
