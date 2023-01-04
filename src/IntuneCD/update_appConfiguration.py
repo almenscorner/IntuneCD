62228cf53b96f11fb4d748e41ec70701c2e8fc0d
Tobias Almén <almenscorner@outlook.com>
2023-01-04 10:49:27 +0100

    Add report option

--- src/IntuneCD/update_appConfiguration.py
+++ src/IntuneCD/update_appConfiguration.py
@@ -17,11 +17,13 @@ from .check_file import check_file
 from .load_file import load_file
 
 # Set MS Graph endpoint
-ENDPOINT = "https://graph.microsoft.com/beta/deviceAppManagement/mobileAppConfigurations"
+ENDPOINT = (
+    "https://graph.microsoft.com/beta/deviceAppManagement/mobileAppConfigurations"
+)
 APP_ENDPOINT = "https://graph.microsoft.com/beta/deviceAppManagement/mobileApps"
 
 
-def update(path, token, assignment=False):
+def update(path, token, assignment=False, report=False):
     """
     This function updates all App Configuration Polices in Intune,
     if the configuration in Intune differs from the JSON/YAML file.
@@ -66,7 +68,10 @@ def update(path, token, assignment=False):
                 data = {"value": ""}
                 if mem_data["value"]:
                     for val in mem_data["value"]:
-                        if repo_data["@odata.type"] == val["@odata.type"] and repo_data["displayName"] == val["displayName"]:
+                        if (
+                            repo_data["@odata.type"] == val["@odata.type"]
+                            and repo_data["displayName"] == val["displayName"]
+                        ):
                             data["value"] = val
 
                 if data["value"]:
@@ -76,10 +81,12 @@ def update(path, token, assignment=False):
                     data = remove_keys(data)
                     repo_data.pop("targetedMobileApps", None)
 
-                    diff = DeepDiff(data["value"], repo_data, ignore_order=True).get("values_changed", {})
+                    diff = DeepDiff(data["value"], repo_data, ignore_order=True).get(
+                        "values_changed", {}
+                    )
 
                     # If any changed values are found, push them to Intune
-                    if diff:
+                    if diff and report is False:
                         request_data = json.dumps(repo_data)
                         q_param = None
                         makeapirequestPatch(
@@ -114,14 +121,19 @@ def update(path, token, assignment=False):
                 # If App Configuration does not exist, create it and assign
                 else:
                     print("-" * 90)
-                    print("App Configuration not found, creating: " + repo_data["displayName"])
+                    print(
+                        "App Configuration not found, creating: "
+                        + repo_data["displayName"]
+                    )
                     app_ids = {}
                     # If backup contains targeted apps, search for the app
                     if repo_data["targetedMobileApps"]:
                         q_param = {
                             "$filter": "(isof("
                             + "'"
-                            + str(repo_data["targetedMobileApps"]["type"]).replace("#", "")
+                            + str(repo_data["targetedMobileApps"]["type"]).replace(
+                                "#", ""
+                            )
                             + "'"
                             + "))",
                             "$search": repo_data["targetedMobileApps"]["appName"],
@@ -131,7 +143,7 @@ def update(path, token, assignment=False):
                             app_ids = app_request["value"][0]["id"]
                     # If the app could be found and matches type and name in
                     # backup, continue to create
-                    if app_ids:
+                    if app_ids and report is False:
                         repo_data.pop("targetedMobileApps")
                         repo_data["targetedMobileApps"] = [app_ids]
                         request_json = json.dumps(repo_data)
@@ -143,7 +155,9 @@ def update(path, token, assignment=False):
                             status_code=201,
                         )
                         mem_assign_obj = []
-                        assignment = update_assignment(assign_obj, mem_assign_obj, token)
+                        assignment = update_assignment(
+                            assign_obj, mem_assign_obj, token
+                        )
                         if assignment is not None:
                             request_data = {"assignments": assignment}
                             post_assignment_update(
@@ -153,8 +167,12 @@ def update(path, token, assignment=False):
                                 "/microsoft.graph.managedDeviceMobileAppConfiguration/assign",
                                 token,
                             )
-                        print("App Configuration created with id: " + post_request["id"])
+                        print(
+                            "App Configuration created with id: " + post_request["id"]
+                        )
                     else:
-                        print("App configured in App Configuration profile could not be found, skipping creation")
+                        print(
+                            "App configured in App Configuration profile could not be found, skipping creation"
+                        )
 
     return diff_summary
