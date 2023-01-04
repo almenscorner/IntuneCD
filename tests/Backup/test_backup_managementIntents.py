b8c50ac585d7c2b034bea2ed8fda5e7fad3477fd
Tobias Almén <almenscorner@outlook.com>
2023-01-04 09:00:55 +0100

    Black code style

--- tests/Backup/test_backup_managementIntents.py
+++ tests/Backup/test_backup_managementIntents.py
@@ -11,17 +11,8 @@ from unittest.mock import patch
 from src.IntuneCD.backup_managementIntents import savebackup
 from testfixtures import TempDirectory
 
-BATCH_ASSIGNMENT = [
-    {
-        'value': [
-            {
-                'id': '0',
-                'target': {
-                    'groupName': 'Group1'}}]}]
-OBJECT_ASSIGNMENT = [
-    {
-        'target': {
-            'groupName': 'Group1'}}]
+BATCH_ASSIGNMENT = [{"value": [{"id": "0", "target": {"groupName": "Group1"}}]}]
+OBJECT_ASSIGNMENT = [{"target": {"groupName": "Group1"}}]
 
 
 class TestBackupManagementIntent(unittest.TestCase):
@@ -30,51 +21,59 @@ class TestBackupManagementIntent(unittest.TestCase):
     def setUp(self):
         self.directory = TempDirectory()
         self.directory.create()
-        self.token = 'token'
+        self.token = "token"
         self.exclude = []
         self.saved_path = f"{self.directory.path}/Management Intents/Dummy Intent/test."
         self.expected_data = {
-            'assignments': [{'target': {'groupName': 'Group1'}}],
-            'description': 'Hi there',
+            "assignments": [{"target": {"groupName": "Group1"}}],
+            "description": "Hi there",
             "displayName": "test",
             "templateId": "0",
             "roleScopeTagIds": ["0"],
-            "settingsDelta": [{"value": True}]
-        }
-        self.intent = {"value": [{
-            "displayName": "test",
-            "id": "0",
-        }]}
-        self.template = {"value": [{
-            "displayName": "Dummy Intent",
-            "id": "0",
-        }]}
-        self.batch_intent_data = {"value": [{
-            "id": "0",
-            "displayName": "test",
-            "description": "Hi there",
-            "templateId": "0",
             "settingsDelta": [{"value": True}],
-            "roleScopeTagIds": ["0"]
-        }]}
+        }
+        self.intent = {
+            "value": [
+                {
+                    "displayName": "test",
+                    "id": "0",
+                }
+            ]
+        }
+        self.template = {
+            "value": [
+                {
+                    "displayName": "Dummy Intent",
+                    "id": "0",
+                }
+            ]
+        }
+        self.batch_intent_data = {
+            "value": [
+                {
+                    "id": "0",
+                    "displayName": "test",
+                    "description": "Hi there",
+                    "templateId": "0",
+                    "settingsDelta": [{"value": True}],
+                    "roleScopeTagIds": ["0"],
+                }
+            ]
+        }
 
-        self.batch_intent_patch = patch(
-            'src.IntuneCD.backup_managementIntents.batch_intents')
+        self.batch_intent_patch = patch("src.IntuneCD.backup_managementIntents.batch_intents")
         self.batch_intent = self.batch_intent_patch.start()
         self.batch_intent.return_value = self.batch_intent_data
 
-        self.batch_assignment_patch = patch(
-            'src.IntuneCD.backup_managementIntents.batch_assignment')
+        self.batch_assignment_patch = patch("src.IntuneCD.backup_managementIntents.batch_assignment")
         self.batch_assignment = self.batch_assignment_patch.start()
         self.batch_assignment.return_value = BATCH_ASSIGNMENT
 
-        self.object_assignment_patch = patch(
-            'src.IntuneCD.backup_managementIntents.get_object_assignment')
+        self.object_assignment_patch = patch("src.IntuneCD.backup_managementIntents.get_object_assignment")
         self.object_assignment = self.object_assignment_patch.start()
         self.object_assignment.return_value = OBJECT_ASSIGNMENT
 
-        self.makeapirequest_patch = patch(
-            'src.IntuneCD.backup_managementIntents.makeapirequest')
+        self.makeapirequest_patch = patch("src.IntuneCD.backup_managementIntents.makeapirequest")
         self.makeapirequest = self.makeapirequest_patch.start()
         self.makeapirequest.side_effect = self.intent, self.template
 
@@ -88,35 +87,25 @@ class TestBackupManagementIntent(unittest.TestCase):
     def test_backup_yml(self):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
 
-        self.count = savebackup(
-            self.directory.path,
-            'yaml',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "yaml", self.exclude, self.token)
 
-        with open(self.saved_path + 'yaml', 'r') as f:
+        with open(self.saved_path + "yaml", "r") as f:
             data = json.dumps(yaml.safe_load(f))
             saved_data = json.loads(data)
 
-        self.assertTrue(
-            Path(f'{self.directory.path}/Management Intents/Dummy Intent').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Management Intents/Dummy Intent").exists())
         self.assertEqual(self.expected_data, saved_data)
         self.assertEqual(1, self.count)
 
     def test_backup_json(self):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
 
-        self.count = savebackup(
-            self.directory.path,
-            'json',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "json", self.exclude, self.token)
 
-        with open(self.saved_path + 'json', 'r') as f:
+        with open(self.saved_path + "json", "r") as f:
             saved_data = json.load(f)
 
-        self.assertTrue(
-            Path(f'{self.directory.path}/Management Intents/Dummy Intent').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Management Intents/Dummy Intent").exists())
         self.assertEqual(self.expected_data, saved_data)
         self.assertEqual(1, self.count)
 
@@ -124,14 +113,10 @@ class TestBackupManagementIntent(unittest.TestCase):
         """The count should be 0 if no data is returned."""
 
         self.batch_intent.return_value = {"value": []}
-        self.count = savebackup(
-            self.directory.path,
-            'json',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "json", self.exclude, self.token)
 
         self.assertEqual(0, self.count)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
