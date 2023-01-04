03c0932e6e87ccf14045fd6dec2277bc15f4cf7a
Tobias Almén <almenscorner@outlook.com>
2023-01-04 09:01:04 +0100

    Black code style

--- tests/Backup/test_backup_powershellScripts.py
+++ tests/Backup/test_backup_powershellScripts.py
@@ -11,17 +11,8 @@ from unittest.mock import patch
 from src.IntuneCD.backup_powershellScripts import savebackup
 from testfixtures import TempDirectory
 
-BATCH_ASSIGNMENT = [
-    {
-        'value': [
-            {
-                'id': '0',
-                'target': {
-                    'groupName': 'Group1'}}]}]
-OBJECT_ASSIGNMENT = [
-    {
-        'target': {
-            'groupName': 'Group1'}}]
+BATCH_ASSIGNMENT = [{"value": [{"id": "0", "target": {"groupName": "Group1"}}]}]
+OBJECT_ASSIGNMENT = [{"target": {"groupName": "Group1"}}]
 
 
 class TestBackupPowershellScript(unittest.TestCase):
@@ -30,44 +21,39 @@ class TestBackupPowershellScript(unittest.TestCase):
     def setUp(self):
         self.directory = TempDirectory()
         self.directory.create()
-        self.token = 'token'
+        self.token = "token"
         self.exclude = []
         self.saved_path = f"{self.directory.path}/Scripts/Powershell/test."
         self.script_content_path = f"{self.directory.path}/Scripts/Powershell/Script Data/test.ps1"
         self.expected_data = {
-            'assignments': [
-                {
-                    'target': {
-                        'groupName': 'Group1'}}],
-            'displayName': 'test',
-            'fileName': 'test',
-            'scriptContent': 'WW91IGZvdW5kIGEgc2VjcmV0IG1lc3NhZ2UsIGhvb3JheSE='}
-        self.script_policy_data = {
-            "value": [{"id": "0", "displayName": "test"}]}
+            "assignments": [{"target": {"groupName": "Group1"}}],
+            "displayName": "test",
+            "fileName": "test",
+            "scriptContent": "WW91IGZvdW5kIGEgc2VjcmV0IG1lc3NhZ2UsIGhvb3JheSE=",
+        }
+        self.script_policy_data = {"value": [{"id": "0", "displayName": "test"}]}
         self.batch_request_data = [
             {
-                'id': '0',
-                'displayName': 'test',
-                'fileName': 'test',
-                'scriptContent': 'WW91IGZvdW5kIGEgc2VjcmV0IG1lc3NhZ2UsIGhvb3JheSE='}]
+                "id": "0",
+                "displayName": "test",
+                "fileName": "test",
+                "scriptContent": "WW91IGZvdW5kIGEgc2VjcmV0IG1lc3NhZ2UsIGhvb3JheSE=",
+            }
+        ]
 
-        self.batch_assignment_patch = patch(
-            'src.IntuneCD.backup_powershellScripts.batch_assignment')
+        self.batch_assignment_patch = patch("src.IntuneCD.backup_powershellScripts.batch_assignment")
         self.batch_assignment = self.batch_assignment_patch.start()
         self.batch_assignment.return_value = BATCH_ASSIGNMENT
 
-        self.object_assignment_patch = patch(
-            'src.IntuneCD.backup_powershellScripts.get_object_assignment')
+        self.object_assignment_patch = patch("src.IntuneCD.backup_powershellScripts.get_object_assignment")
         self.object_assignment = self.object_assignment_patch.start()
         self.object_assignment.return_value = OBJECT_ASSIGNMENT
 
-        self.batch_request_patch = patch(
-            'src.IntuneCD.backup_powershellScripts.batch_request')
+        self.batch_request_patch = patch("src.IntuneCD.backup_powershellScripts.batch_request")
         self.batch_request = self.batch_request_patch.start()
         self.batch_request.return_value = self.batch_request_data
 
-        self.makeapirequest_patch = patch(
-            'src.IntuneCD.backup_powershellScripts.makeapirequest')
+        self.makeapirequest_patch = patch("src.IntuneCD.backup_powershellScripts.makeapirequest")
         self.makeapirequest = self.makeapirequest_patch.start()
         self.makeapirequest.return_value = self.script_policy_data
 
@@ -81,50 +67,35 @@ class TestBackupPowershellScript(unittest.TestCase):
     def test_backup_yml(self):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
 
-        self.count = savebackup(
-            self.directory.path,
-            'yaml',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "yaml", self.exclude, self.token)
 
-        with open(self.saved_path + 'yaml', 'r') as f:
+        with open(self.saved_path + "yaml", "r") as f:
             data = json.dumps(yaml.safe_load(f))
             self.saved_data = json.loads(data)
 
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
-        self.assertTrue(
-            Path(f'{self.directory.path}/Scripts/Powershell').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Scripts/Powershell").exists())
         self.assertEqual(self.expected_data, self.saved_data)
         self.assertEqual(1, self.count)
 
     def test_backup_json(self):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
 
-        self.count = savebackup(
-            self.directory.path,
-            'json',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "json", self.exclude, self.token)
 
-        with open(self.saved_path + 'json', 'r') as f:
+        with open(self.saved_path + "json", "r") as f:
             self.saved_data = json.load(f)
 
-        self.assertTrue(
-            Path(f'{self.directory.path}/Scripts/Powershell').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Scripts/Powershell").exists())
         self.assertEqual(self.expected_data, self.saved_data)
         self.assertEqual(1, self.count)
 
     def test_script_is_created(self):
         """The script data folder should be created and a .ps1 file should be created."""
 
-        self.count = savebackup(
-            self.directory.path,
-            'json',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "json", self.exclude, self.token)
 
-        self.assertTrue(
-            Path(f'{self.directory.path}/Scripts/Powershell/Script Data').exists())
+        self.assertTrue(Path(f"{self.directory.path}/Scripts/Powershell/Script Data").exists())
         self.assertTrue(self.script_content_path)
         self.assertEqual(1, self.count)
 
@@ -132,14 +103,10 @@ class TestBackupPowershellScript(unittest.TestCase):
         """The count should be 0 if no data is returned."""
 
         self.makeapirequest.return_value = {"value": []}
-        self.count = savebackup(
-            self.directory.path,
-            'json',
-            self.exclude,
-            self.token)
+        self.count = savebackup(self.directory.path, "json", self.exclude, self.token)
 
         self.assertEqual(0, self.count)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
