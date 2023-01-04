6838b84119917110e8e86749248322f7cd4ed7f5
Tobias Almén <almenscorner@outlook.com>
2023-01-04 08:57:27 +0100

    Add change summary frontend update

--- src/IntuneCD/run_update.py
+++ src/IntuneCD/run_update.py
@@ -23,6 +23,7 @@ import os
 import sys
 import base64
 import argparse
+import json
 
 from io import StringIO
 from .get_authparams import getAuth
@@ -32,12 +33,11 @@ REPO_DIR = os.environ.get("REPO_DIR")
 
 
 def start():
-    parser = argparse.ArgumentParser(
-        description="Update Intune configurations with values from backup")
+    parser = argparse.ArgumentParser(description="Update Intune configurations with values from backup")
     parser.add_argument(
         "-p",
         "--path",
-        help='The path to which the configurations are saved. Default value is $(Build.SourcesDirectory)',
+        help="The path to which the configurations are saved. Default value is $(Build.SourcesDirectory)",
         default=REPO_DIR,
     )
     parser.add_argument(
@@ -46,26 +46,32 @@ def start():
         help=(
             "The mode in which the script is run, 0 = devtoprod (backup from dev -> update to prod) "
             "uses os.environ PROD_TENANT_NAME, PROD_CLIENT_ID, PROD_CLIENT_SECRET, "
-            "1 = standalone (backup from prod) uses os.environ TENANT_NAME, CLIENT_ID, CLIENT_SECRET"),
+            "1 = standalone (backup from prod) uses os.environ TENANT_NAME, CLIENT_ID, CLIENT_SECRET"
+        ),
         type=int,
-        default=0)
+        default=0,
+    )
     parser.add_argument(
         "-a",
         "--localauth",
         help=(
             "When this paramater is set, provide a path to a local dict file containing the following keys: "
             "params:TENANT_NAME, CLIENT_ID, CLIENT_SECRET when run in standalone mode and "
-            "params:PROD_TENANT_NAME, PROD_CLIENT_ID, PROD_CLIENT_SECRET when run in devtoprod"),
-        type=str)
+            "params:PROD_TENANT_NAME, PROD_CLIENT_ID, PROD_CLIENT_SECRET when run in devtoprod"
+        ),
+        type=str,
+    )
     parser.add_argument(
         "-u",
         help="When this parameter is set, assignments are updated for all configurations",
-        action="store_true")
+        action="store_true",
+    )
     parser.add_argument(
         "-f",
         "--frontend",
         help="Set the frontend URL to update with configuration count and backup stream",
-        type=str)
+        type=str,
+    )
     parser.add_argument(
         "-e",
         "--exclude",
@@ -85,8 +91,10 @@ def start():
             "PowershellScripts",
             "ShellScripts",
             "ConfigurationPolicies",
-            "ConditionalAccess"],
-        nargs='+')
+            "ConditionalAccess",
+        ],
+        nargs="+",
+    )
 
     args = parser.parse_args()
 
@@ -96,10 +104,7 @@ def start():
     def standalone():
         return "standalone"
 
-    switcher = {
-        0: devtoprod,
-        1: standalone
-    }
+    switcher = {0: devtoprod, 1: standalone}
 
     def selected_mode(argument):
         func = switcher.get(argument, "nothing")
@@ -110,68 +115,88 @@ def start():
     def run_update(path, token, assignment, exclude):
 
         diff_count = 0
+        diff_summary = []
 
         if "AppConfigurations" not in exclude:
             from .update_appConfiguration import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "AppProtection" not in exclude:
             from .update_appProtection import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "Compliance" not in exclude:
             from .update_compliance import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "NotificationTemplate" not in exclude:
             from .update_notificationTemplate import update
-            diff_count += update(path, token)
+
+            diff_summary.append(update(path, token))
 
         if "Profiles" not in exclude:
             from .update_profiles import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "AppleEnrollmentProfile" not in exclude:
             from .update_appleEnrollmentProfile import update
-            diff_count += update(path, token)
+
+            diff_summary.append(update(path, token))
 
         if "WindowsEnrollmentProfile" not in exclude:
             from .update_windowsEnrollmentProfile import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "EnrollmentStatusPage" not in exclude:
             from .update_enrollmentStatusPage import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "Filters" not in exclude:
             from .update_assignmentFilter import update
-            diff_count += update(path, token)
+
+            diff_summary.append(update(path, token))
 
         if "Intents" not in exclude:
             from .update_managementIntents import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "ProactiveRemediation" not in exclude:
             from .update_proactiveRemediation import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "PowershellScripts" not in exclude:
             from .update_powershellScripts import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "ShellScripts" not in exclude:
             from .update_shellScripts import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "ConfigurationPolicies" not in exclude:
             from .update_configurationPolicies import update
-            diff_count += update(path, token, assignment)
+
+            diff_summary.append(update(path, token, assignment))
 
         if "ConditionalAccess" not in exclude:
             from .update_conditionalAccess import update
+
             diff_count += update(path, token)
 
-        return diff_count
+        for sum in diff_summary:
+            for config in sum:
+                diff_count += config.count
+
+        return diff_count, diff_summary
 
     if token is None:
         raise Exception("Token is empty, please check os.environ variables")
@@ -186,22 +211,30 @@ def start():
 
             old_stdout = sys.stdout
             sys.stdout = feedstdout = StringIO()
-            count = run_update(args.path, token, args.u, exclude)
+            summary = run_update(args.path, token, args.u, exclude)
             sys.stdout = old_stdout
             feed_bytes = feedstdout.getvalue().encode("utf-8")
             out = base64.b64encode(feed_bytes).decode("utf-8")
 
-            body = {
-                "type": "diff_count",
-                "diff_count": count
-            }
-            update_frontend(f'{args.frontend}/api/overview/summary', body)
+            body = {"type": "diff_count", "diff_count": summary[0]}
+            update_frontend(f"{args.frontend}/api/overview/summary", body)
 
-            body = {
-                "type": "update",
-                "feed": out
-            }
-            update_frontend(f'{args.frontend}/api/feed/update', body)
+            body = {"type": "update", "feed": out}
+            update_frontend(f"{args.frontend}/api/feed/update", body)
+
+            body = []
+            for sum in summary[1]:
+                for config in sum:
+                    if config.diffs:
+                        body.append(
+                            {
+                                "name": config.name,
+                                "diffs": config.diffs,
+                            }
+                        )
+
+            if len(body) > 0:
+                update_frontend(f"{args.frontend}/api/changes/summary", body)
 
         else:
             run_update(args.path, token, args.u, exclude)
