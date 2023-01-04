cff2718e72a017b10465c2af7877288a4d4d1fb2
Tobias Almén <almenscorner@outlook.com>
2023-01-04 09:03:06 +0100

    Update count to DiffSummary count

--- tests/Update/test_update_assignmentFilter.py
+++ tests/Update/test_update_assignmentFilter.py
@@ -12,40 +12,26 @@ class TestUpdateAssignmentFilter(unittest.TestCase):
         self.directory = TempDirectory()
         self.directory.create()
         self.directory.makedir("Filters")
-        self.directory.write(
-            "Filters/test.json", '{"test": "test"}',
-            encoding='utf-8')
-        self.directory.write(
-            "Filters/test.txt", '{"test": "test"}',
-            encoding='utf-8')
-        self.token = 'token'
-        self.mem_data = {"value": [{"@odata.type": "test",
-                                    "id": "0",
-                                    "displayName": "test",
-                                    "testvalue": "test"}]}
-        self.repo_data = {"@odata.type": "test",
-                          "id": "0",
-                          "displayName": "test",
-                          "testvalue": "test1"}
+        self.directory.write("Filters/test.json", '{"test": "test"}', encoding="utf-8")
+        self.directory.write("Filters/test.txt", '{"test": "test"}', encoding="utf-8")
+        self.token = "token"
+        self.mem_data = {"value": [{"@odata.type": "test", "id": "0", "displayName": "test", "testvalue": "test"}]}
+        self.repo_data = {"@odata.type": "test", "id": "0", "displayName": "test", "testvalue": "test1"}
 
-        self.makeapirequest_patch = patch(
-            'src.IntuneCD.update_assignmentFilter.makeapirequest')
+        self.makeapirequest_patch = patch("src.IntuneCD.update_assignmentFilter.makeapirequest")
         self.makeapirequest = self.makeapirequest_patch.start()
         self.makeapirequest.return_value = self.mem_data
 
-        self.load_file_patch = patch(
-            'src.IntuneCD.update_assignmentFilter.load_file')
+        self.load_file_patch = patch("src.IntuneCD.update_assignmentFilter.load_file")
         self.load_file = self.load_file_patch.start()
         self.load_file.return_value = self.repo_data
 
-        self.makeapirequestPatch_patch = patch(
-            'src.IntuneCD.update_assignmentFilter.makeapirequestPatch')
+        self.makeapirequestPatch_patch = patch("src.IntuneCD.update_assignmentFilter.makeapirequestPatch")
         self.makeapirequestPatch = self.makeapirequestPatch_patch.start()
 
-        self.makeapirequestPost_patch = patch(
-            'src.IntuneCD.update_assignmentFilter.makeapirequestPost')
+        self.makeapirequestPost_patch = patch("src.IntuneCD.update_assignmentFilter.makeapirequestPost")
         self.makeapirequestPost = self.makeapirequestPost_patch.start()
-        self.makeapirequestPost.return_value = {'id': '0'}
+        self.makeapirequestPost.return_value = {"id": "0"}
 
     def tearDown(self):
         self.directory.cleanup()
@@ -59,42 +45,42 @@ class TestUpdateAssignmentFilter(unittest.TestCase):
 
         self.count = update(self.directory.path, self.token)
 
-        self.assertEqual(self.count, 1)
+        self.assertEqual(self.count[0].count, 1)
         self.assertEqual(self.makeapirequestPatch.call_count, 1)
         self.assertEqual(self.makeapirequestPatch.call_count, 1)
 
     def test_update_with_multiple_diffs(self):
         """The count should be 1 and makeapirequestPatch should be called."""
 
-        self.repo_data['testvalue2'] = 'test2'
-        self.mem_data['value'][0]['testvalue'] = 'test'
-        self.mem_data['value'][0]['testvalue2'] = 'test1'
+        self.repo_data["testvalue2"] = "test2"
+        self.mem_data["value"][0]["testvalue"] = "test"
+        self.mem_data["value"][0]["testvalue2"] = "test1"
 
         self.count = update(self.directory.path, self.token)
 
-        self.assertEqual(self.count, 1)
+        self.assertEqual(self.count[0].count, 2)
         self.assertEqual(self.makeapirequestPatch.call_count, 1)
         self.assertEqual(self.makeapirequestPatch.call_count, 1)
 
     def test_update_with_no_diffs(self):
         """The count should be 0 and makeapirequestPatch should not be called."""
 
-        self.mem_data['value'][0]['testvalue'] = 'test1'
+        self.mem_data["value"][0]["testvalue"] = "test1"
         self.count = update(self.directory.path, self.token)
 
-        self.assertEqual(self.count, 0)
+        self.assertEqual(self.count[0].count, 0)
         self.assertEqual(self.makeapirequestPatch.call_count, 0)
         self.assertEqual(self.makeapirequestPatch.call_count, 0)
 
     def test_update_config_not_found(self):
         """The count should be 0 and makeapirequestPost should be called."""
 
-        self.mem_data['value'][0]['displayName'] = 'test1'
+        self.mem_data["value"][0]["displayName"] = "test1"
         self.count = update(self.directory.path, self.token)
 
-        self.assertEqual(self.count, 0)
+        self.assertEqual(self.count, [])
         self.assertEqual(self.makeapirequestPost.call_count, 1)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
