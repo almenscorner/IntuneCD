97fd84bd972c31a902dc160acb863bdb13b6f5e8
Tobias Almén <almenscorner@outlook.com>
2023-01-04 10:49:45 +0100

    Add report option

--- src/IntuneCD/update_configurationPolicies.py
+++ src/IntuneCD/update_configurationPolicies.py
@@ -19,7 +19,7 @@ from .diff_summary import DiffSummary
 ENDPOINT = "https://graph.microsoft.com/beta/deviceManagement/configurationPolicies"
 
 
-def update(path, token, assignment=False):
+def update(path, token, assignment=False, report=False):
     """
     This function updates all Settings Catalog configurations in Intune,
     if the configuration in Intune differs from the JSON/YAML file.
@@ -37,7 +37,9 @@ def update(path, token, assignment=False):
         # Get configurations policies
         mem_data = makeapirequest(ENDPOINT, token)
         # Get current assignments
-        mem_assignments = batch_assignment(mem_data, "deviceManagement/configurationPolicies/", "/assignments", token)
+        mem_assignments = batch_assignment(
+            mem_data, "deviceManagement/configurationPolicies/", "/assignments", token
+        )
 
         for filename in os.listdir(configpath):
             file = check_file(configpath, filename)
@@ -65,16 +67,22 @@ def update(path, token, assignment=False):
                 if data["value"]:
                     print("-" * 90)
                     # Get Filter data from Intune
-                    mem_policy_data = makeapirequest(ENDPOINT + "/" + data["value"]["id"], token)
+                    mem_policy_data = makeapirequest(
+                        ENDPOINT + "/" + data["value"]["id"], token
+                    )
                     # Get Filter settings from Intune
-                    mem_policy_settings = makeapirequest(ENDPOINT + "/" + data["value"]["id"] + "/settings", token)
+                    mem_policy_settings = makeapirequest(
+                        ENDPOINT + "/" + data["value"]["id"] + "/settings", token
+                    )
                     # Add settings to the data dictionary
                     mem_policy_data["settings"] = mem_policy_settings["value"]
 
-                    diff = DeepDiff(mem_policy_data, repo_data, ignore_order=True).get("values_changed", {})
+                    diff = DeepDiff(mem_policy_data, repo_data, ignore_order=True).get(
+                        "values_changed", {}
+                    )
 
                     # If any changed values are found, push them to Intune
-                    if diff:
+                    if diff and report is False:
                         request_data = json.dumps(repo_data)
                         q_param = None
                         makeapirequestPut(
@@ -94,7 +102,9 @@ def update(path, token, assignment=False):
                     diff_summary.append(diff_policy)
 
                     if assignment:
-                        mem_assign_obj = get_object_assignment(data["value"]["id"], mem_assignments)
+                        mem_assign_obj = get_object_assignment(
+                            data["value"]["id"], mem_assignments
+                        )
                         update = update_assignment(assign_obj, mem_assign_obj, token)
                         if update is not None:
                             request_data = {"assignments": update}
@@ -109,27 +119,36 @@ def update(path, token, assignment=False):
                 # If Configuration Policy does not exist, create it and assign
                 else:
                     print("-" * 90)
-                    print("Configuration Policy not found, creating Policy: " + repo_data["name"])
-                    repo_data.pop("settingCount", None)
-                    request_json = json.dumps(repo_data)
-                    post_request = makeapirequestPost(
-                        ENDPOINT,
-                        token,
-                        q_param=None,
-                        jdata=request_json,
-                        status_code=201,
+                    print(
+                        "Configuration Policy not found, creating Policy: "
+                        + repo_data["name"]
                     )
-                    mem_assign_obj = []
-                    assignment = update_assignment(assign_obj, mem_assign_obj, token)
-                    if assignment is not None:
-                        request_data = {"assignments": assignment}
-                        post_assignment_update(
-                            request_data,
-                            post_request["id"],
-                            "deviceManagement/configurationPolicies",
-                            "assign",
+                    if report is False:
+                        repo_data.pop("settingCount", None)
+                        request_json = json.dumps(repo_data)
+                        post_request = makeapirequestPost(
+                            ENDPOINT,
                             token,
+                            q_param=None,
+                            jdata=request_json,
+                            status_code=201,
                         )
-                    print("Configuration Policy created with id: " + post_request["id"])
+                        mem_assign_obj = []
+                        assignment = update_assignment(
+                            assign_obj, mem_assign_obj, token
+                        )
+                        if assignment is not None:
+                            request_data = {"assignments": assignment}
+                            post_assignment_update(
+                                request_data,
+                                post_request["id"],
+                                "deviceManagement/configurationPolicies",
+                                "assign",
+                                token,
+                            )
+                        print(
+                            "Configuration Policy created with id: "
+                            + post_request["id"]
+                        )
 
     return diff_summary
