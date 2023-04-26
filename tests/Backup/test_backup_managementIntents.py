0ebdb1b55bccbab4d7c9f0df51205dec921b21dd
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-04-26 13:07:20 +0200

    Update for changed return of config count

--- tests/Backup/test_backup_managementIntents.py
+++ tests/Backup/test_backup_managementIntents.py
@@ -61,19 +61,27 @@ class TestBackupManagementIntent(unittest.TestCase):
             ]
         }
 
-        self.batch_intent_patch = patch("src.IntuneCD.backup_managementIntents.batch_intents")
+        self.batch_intent_patch = patch(
+            "src.IntuneCD.backup_managementIntents.batch_intents"
+        )
         self.batch_intent = self.batch_intent_patch.start()
         self.batch_intent.return_value = self.batch_intent_data
 
-        self.batch_assignment_patch = patch("src.IntuneCD.backup_managementIntents.batch_assignment")
+        self.batch_assignment_patch = patch(
+            "src.IntuneCD.backup_managementIntents.batch_assignment"
+        )
         self.batch_assignment = self.batch_assignment_patch.start()
         self.batch_assignment.return_value = BATCH_ASSIGNMENT
 
-        self.object_assignment_patch = patch("src.IntuneCD.backup_managementIntents.get_object_assignment")
+        self.object_assignment_patch = patch(
+            "src.IntuneCD.backup_managementIntents.get_object_assignment"
+        )
         self.object_assignment = self.object_assignment_patch.start()
         self.object_assignment.return_value = OBJECT_ASSIGNMENT
 
-        self.makeapirequest_patch = patch("src.IntuneCD.backup_managementIntents.makeapirequest")
+        self.makeapirequest_patch = patch(
+            "src.IntuneCD.backup_managementIntents.makeapirequest"
+        )
         self.makeapirequest = self.makeapirequest_patch.start()
         self.makeapirequest.side_effect = self.intent, self.template
 
@@ -93,9 +101,11 @@ class TestBackupManagementIntent(unittest.TestCase):
             data = json.dumps(yaml.safe_load(f))
             saved_data = json.loads(data)
 
-        self.assertTrue(Path(f"{self.directory.path}/Management Intents/Dummy Intent").exists())
+        self.assertTrue(
+            Path(f"{self.directory.path}/Management Intents/Dummy Intent").exists()
+        )
         self.assertEqual(self.expected_data, saved_data)
-        self.assertEqual(1, self.count)
+        self.assertEqual(1, self.count["config_count"])
 
     def test_backup_json(self):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
@@ -105,9 +115,11 @@ class TestBackupManagementIntent(unittest.TestCase):
         with open(self.saved_path + "json", "r") as f:
             saved_data = json.load(f)
 
-        self.assertTrue(Path(f"{self.directory.path}/Management Intents/Dummy Intent").exists())
+        self.assertTrue(
+            Path(f"{self.directory.path}/Management Intents/Dummy Intent").exists()
+        )
         self.assertEqual(self.expected_data, saved_data)
-        self.assertEqual(1, self.count)
+        self.assertEqual(1, self.count["config_count"])
 
     def test_backup_with_no_returned_data(self):
         """The count should be 0 if no data is returned."""
@@ -115,7 +127,7 @@ class TestBackupManagementIntent(unittest.TestCase):
         self.batch_intent.return_value = {"value": []}
         self.count = savebackup(self.directory.path, "json", self.exclude, self.token)
 
-        self.assertEqual(0, self.count)
+        self.assertEqual(0, self.count["config_count"])
 
 
 if __name__ == "__main__":
