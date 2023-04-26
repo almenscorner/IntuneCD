4b9c9874bced552ef6d473612a028af2eed4b551
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-04-26 13:08:56 +0200

    Add makeapirequestDelete Patch

--- tests/Update/test_update_ManagementIntents.py
+++ tests/Update/test_update_ManagementIntents.py
@@ -16,13 +16,26 @@ class TestUpdateManagementIntents(unittest.TestCase):
         self.directory = TempDirectory()
         self.directory.create()
         self.directory.makedir("Management Intents")
-        self.directory.write("Management Intents/macOS Firewall/test.json", '{"test": "test"}', encoding="utf-8")
-        self.directory.write("Management Intents/macOS Firewall/.DS_Store", '', encoding="utf-8")
-        self.directory.write("Management Intents/macOS Firewall/test.md", '', encoding="utf-8")
+        self.directory.write(
+            "Management Intents/macOS Firewall/test.json",
+            '{"test": "test"}',
+            encoding="utf-8",
+        )
+        self.directory.write(
+            "Management Intents/macOS Firewall/.DS_Store", "", encoding="utf-8"
+        )
+        self.directory.write(
+            "Management Intents/macOS Firewall/test.md", "", encoding="utf-8"
+        )
         self.token = "token"
         self.mem_data = {
             "value": [
-                {"@odata.type": "test", "id": "0", "displayName": "test", "assignments": [{"target": {"groupId": "test"}}]}
+                {
+                    "@odata.type": "test",
+                    "id": "0",
+                    "displayName": "test",
+                    "assignments": [{"target": {"groupId": "test"}}],
+                }
             ]
         }
         self.repo_data = {
@@ -30,7 +43,13 @@ class TestUpdateManagementIntents(unittest.TestCase):
             "displayName": "test",
             "description": "Hi there",
             "templateId": "0",
-            "settingsDelta": [{"@odata.type": "#test.test", "definitionId": "test_test", "value": False}],
+            "settingsDelta": [
+                {
+                    "@odata.type": "#test.test",
+                    "definitionId": "test_test",
+                    "value": False,
+                }
+            ],
             "assignments": [{"target": {"groupName": "test1"}}],
         }
         self.batch_intent_data = {
@@ -40,38 +59,64 @@ class TestUpdateManagementIntents(unittest.TestCase):
                     "displayName": "test",
                     "description": "Hi there",
                     "templateId": "0",
-                    "settingsDelta": [{"id": "0", "@odata.type": "#test.test", "definitionId": "test_test", "value": True}],
+                    "settingsDelta": [
+                        {
+                            "id": "0",
+                            "@odata.type": "#test.test",
+                            "definitionId": "test_test",
+                            "value": True,
+                        }
+                    ],
                 }
             ]
         }
 
-        self.batch_assignment_patch = patch("src.IntuneCD.update_managementIntents.batch_assignment")
+        self.batch_assignment_patch = patch(
+            "src.IntuneCD.update_managementIntents.batch_assignment"
+        )
         self.batch_assignment = self.batch_assignment_patch.start()
 
-        self.batch_intents_patch = patch("src.IntuneCD.update_managementIntents.batch_intents")
+        self.batch_intents_patch = patch(
+            "src.IntuneCD.update_managementIntents.batch_intents"
+        )
         self.batch_intents = self.batch_intents_patch.start()
         self.batch_intents.return_value = self.batch_intent_data
 
-        self.object_assignment_patch = patch("src.IntuneCD.update_managementIntents.get_object_assignment")
+        self.object_assignment_patch = patch(
+            "src.IntuneCD.update_managementIntents.get_object_assignment"
+        )
         self.object_assignment = self.object_assignment_patch.start()
 
-        self.makeapirequest_patch = patch("src.IntuneCD.update_managementIntents.makeapirequest")
+        self.makeapirequest_patch = patch(
+            "src.IntuneCD.update_managementIntents.makeapirequest"
+        )
         self.makeapirequest = self.makeapirequest_patch.start()
         self.makeapirequest.return_value = self.mem_data
 
-        self.update_assignment_patch = patch("src.IntuneCD.update_managementIntents.update_assignment")
+        self.update_assignment_patch = patch(
+            "src.IntuneCD.update_managementIntents.update_assignment"
+        )
         self.update_assignment = self.update_assignment_patch.start()
 
         self.load_file_patch = patch("src.IntuneCD.update_managementIntents.load_file")
         self.load_file = self.load_file_patch.start()
         self.load_file.return_value = self.repo_data
 
-        self.post_assignment_update_patch = patch("src.IntuneCD.update_managementIntents.post_assignment_update")
+        self.post_assignment_update_patch = patch(
+            "src.IntuneCD.update_managementIntents.post_assignment_update"
+        )
         self.post_assignment_update = self.post_assignment_update_patch.start()
 
-        self.makeapirequestPost_patch = patch("src.IntuneCD.update_managementIntents.makeapirequestPost")
+        self.makeapirequestPost_patch = patch(
+            "src.IntuneCD.update_managementIntents.makeapirequestPost"
+        )
         self.makeapirequestPost = self.makeapirequestPost_patch.start()
         self.makeapirequestPost.return_value = {"id": "0"}
+
+        self.makeapirequestDelete_patch = patch(
+            "src.IntuneCD.update_managementIntents.makeapirequestDelete"
+        )
+        self.makeapirequestDelete = self.makeapirequestDelete_patch.start()
 
     def tearDown(self):
         self.directory.cleanup()
@@ -83,6 +128,7 @@ class TestUpdateManagementIntents(unittest.TestCase):
         self.post_assignment_update.stop()
         self.batch_intents.stop()
         self.makeapirequestPost.stop()
+        self.makeapirequestDelete.stop()
 
     def test_update_with_diffs_and_assignment(self):
         """The count should be 1 and the post_assignment_update and makeapirequestPost should be called."""
