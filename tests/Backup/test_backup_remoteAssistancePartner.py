a51b5069f6df65ac91f4c61b326d2d487d1bfaa0
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-04-26 13:07:39 +0200

    Update for changed return of config count

--- tests/Backup/test_backup_remoteAssistancePartner.py
+++ tests/Backup/test_backup_remoteAssistancePartner.py
@@ -11,11 +11,16 @@ from unittest.mock import patch
 from testfixtures import TempDirectory
 from src.IntuneCD.backup_remoteAssistancePartner import savebackup
 
-REMOTE_ASSISTANCE_PARTNER = {"value": [{"id": "0", "onboardingStatus": "onboarded", "displayName": "test"}]}
+REMOTE_ASSISTANCE_PARTNER = {
+    "value": [{"id": "0", "onboardingStatus": "onboarded", "displayName": "test"}]
+}
 
 
 @patch("src.IntuneCD.backup_remoteAssistancePartner.savebackup")
-@patch("src.IntuneCD.backup_remoteAssistancePartner.makeapirequest", return_value=REMOTE_ASSISTANCE_PARTNER)
+@patch(
+    "src.IntuneCD.backup_remoteAssistancePartner.makeapirequest",
+    return_value=REMOTE_ASSISTANCE_PARTNER,
+)
 class TestBackupCompliancePartner(unittest.TestCase):
     """Test class for backup_remoteAssistancePartner."""
 
@@ -23,7 +28,9 @@ class TestBackupCompliancePartner(unittest.TestCase):
         self.directory = TempDirectory()
         self.directory.create()
         self.token = "token"
-        self.saved_path = f"{self.directory.path}/Partner Connections/Remote Assistance/test."
+        self.saved_path = (
+            f"{self.directory.path}/Partner Connections/Remote Assistance/test."
+        )
         self.expected_data = {"onboardingStatus": "onboarded", "displayName": "test"}
 
     def tearDown(self):
@@ -38,9 +45,13 @@ class TestBackupCompliancePartner(unittest.TestCase):
             data = json.dumps(yaml.safe_load(f))
             self.saved_data = json.loads(data)
 
-        self.assertTrue(Path(f"{self.directory.path}/Partner Connections/Remote Assistance").exists())
+        self.assertTrue(
+            Path(
+                f"{self.directory.path}/Partner Connections/Remote Assistance"
+            ).exists()
+        )
         self.assertEqual(self.expected_data, self.saved_data)
-        self.assertEqual(1, self.count)
+        self.assertEqual(1, self.count["config_count"])
 
     def test_backup_json(self, mock_data, mock_makeapirequest):
         """The folder should be created, the file should have the expected contents, and the count should be 1."""
@@ -50,23 +61,27 @@ class TestBackupCompliancePartner(unittest.TestCase):
         with open(self.saved_path + "json", "r") as f:
             self.saved_data = json.load(f)
 
-        self.assertTrue(Path(f"{self.directory.path}/Partner Connections/Remote Assistance").exists())
+        self.assertTrue(
+            Path(
+                f"{self.directory.path}/Partner Connections/Remote Assistance"
+            ).exists()
+        )
         self.assertEqual(self.expected_data, self.saved_data)
-        self.assertEqual(1, self.count)
+        self.assertEqual(1, self.count["config_count"])
 
     def test_onboarding_status_not_onboarded(self, mock_data, mock_makeapirequest):
         """The count should be 0 if the onboarding status is not onboarded."""
 
         mock_data.return_value = {"value": [{"onboardingStatus": "notOnboarded"}]}
         self.count = savebackup(self.directory.path, "json", self.token)
-        self.assertEqual(0, self.count)
+        self.assertEqual(0, self.count["config_count"])
 
     def test_backup_with_no_return_data(self, mock_data, mock_makeapirequest):
         """The count should be 0 if no data is returned."""
 
         mock_data.return_value = {"value": []}
         self.count = savebackup(self.directory.path, "json", self.token)
-        self.assertEqual(0, self.count)
+        self.assertEqual(0, self.count["config_count"])
 
 
 if __name__ == "__main__":
