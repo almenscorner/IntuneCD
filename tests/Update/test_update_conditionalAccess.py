83a7201d0de43e9580d05e76e21cd891995423fe
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-04-26 13:08:37 +0200

    Add makeapirequestDelete Patch

--- tests/Update/test_update_conditionalAccess.py
+++ tests/Update/test_update_conditionalAccess.py
@@ -16,7 +16,9 @@ class TestUpdateConditionalAccess(unittest.TestCase):
         self.directory = TempDirectory()
         self.directory.create()
         self.directory.makedir("Conditional Access")
-        self.directory.write("Conditional Access/test.json", '{"test": "test"}', encoding="utf-8")
+        self.directory.write(
+            "Conditional Access/test.json", '{"test": "test"}', encoding="utf-8"
+        )
         self.directory.write("Conditional Access/test.txt", "txt", encoding="utf-8")
         self.token = "token"
         self.mem_data = {
@@ -28,17 +30,19 @@ class TestUpdateConditionalAccess(unittest.TestCase):
                         "clientAppTypes": ["all"],
                         "applications": {
                             "includeApplications": ["All"],
-                            "excludedApplications": ["d4ebce55-015a-49b5-a083-c84d1797ae8c"],
+                            "excludedApplications": [
+                                "d4ebce55-015a-49b5-a083-c84d1797ae8c"
+                            ],
                         },
                     },
                     "grantControls": {
-                "operator": "OR",
-                "authenticationStrength": {
-                    "id": "test",
-                    "displayName": "test",
-                    "description": "test",
-                },
-            },
+                        "operator": "OR",
+                        "authenticationStrength": {
+                            "id": "test",
+                            "displayName": "test",
+                            "description": "test",
+                        },
+                    },
                 }
             ]
         }
@@ -48,7 +52,10 @@ class TestUpdateConditionalAccess(unittest.TestCase):
                 "clientAppTypes": ["all"],
                 "applications": {
                     "includeApplications": ["All"],
-                    "excludedApplications": ["d4ebce55-015a-49b5-a083-c84d1797ae8c", "2"],
+                    "excludedApplications": [
+                        "d4ebce55-015a-49b5-a083-c84d1797ae8c",
+                        "2",
+                    ],
                 },
             },
             "grantControls": {
@@ -61,7 +68,9 @@ class TestUpdateConditionalAccess(unittest.TestCase):
             },
         }
 
-        self.makeapirequest_patch = patch("src.IntuneCD.update_conditionalAccess.makeapirequest")
+        self.makeapirequest_patch = patch(
+            "src.IntuneCD.update_conditionalAccess.makeapirequest"
+        )
         self.makeapirequest = self.makeapirequest_patch.start()
         self.makeapirequest.return_value = self.mem_data
 
@@ -69,12 +78,21 @@ class TestUpdateConditionalAccess(unittest.TestCase):
         self.load_file = self.load_file_patch.start()
         self.load_file.return_value = self.repo_data
 
-        self.makeapirequestPatch_patch = patch("src.IntuneCD.update_conditionalAccess.makeapirequestPatch")
+        self.makeapirequestPatch_patch = patch(
+            "src.IntuneCD.update_conditionalAccess.makeapirequestPatch"
+        )
         self.makeapirequestPatch = self.makeapirequestPatch_patch.start()
 
-        self.makeapirequestPost_patch = patch("src.IntuneCD.update_conditionalAccess.makeapirequestPost")
+        self.makeapirequestPost_patch = patch(
+            "src.IntuneCD.update_conditionalAccess.makeapirequestPost"
+        )
         self.makeapirequestPost = self.makeapirequestPost_patch.start()
         self.makeapirequestPost.return_value = {"id": "0"}
+
+        self.makeapirequestDelete_patch = patch(
+            "src.IntuneCD.update_conditionalAccess.makeapirequestDelete"
+        )
+        self.makeapirequestDelete = self.makeapirequestDelete_patch.start()
 
     def tearDown(self):
         self.directory.cleanup()
@@ -82,6 +100,7 @@ class TestUpdateConditionalAccess(unittest.TestCase):
         self.load_file.stop()
         self.makeapirequestPatch.stop()
         self.makeapirequestPost.stop()
+        self.makeapirequestDelete.stop()
 
     def test_update_with_diffs(self):
         """The count should be 1 and the makeapirequestPatch should be called."""
@@ -94,7 +113,9 @@ class TestUpdateConditionalAccess(unittest.TestCase):
     def test_update_with_no_diffs(self):
         """The count should be 0, the post_assignment_update and makeapirequestPatch should not be called."""
 
-        self.repo_data["conditions"]["applications"]["excludedApplications"] = ["d4ebce55-015a-49b5-a083-c84d1797ae8c"]
+        self.repo_data["conditions"]["applications"]["excludedApplications"] = [
+            "d4ebce55-015a-49b5-a083-c84d1797ae8c"
+        ]
 
         self.count = update(self.directory.path, self.token, report=False)
 
@@ -126,7 +147,9 @@ class TestUpdateConditionalAccess(unittest.TestCase):
     def test_update_config_grantControls(self):
         """The count should be 0 and makeapirequestPost should not be called."""
 
-        self.repo_data["conditions"]["applications"]["excludedApplications"] = ["d4ebce55-015a-49b5-a083-c84d1797ae8c"]
+        self.repo_data["conditions"]["applications"]["excludedApplications"] = [
+            "d4ebce55-015a-49b5-a083-c84d1797ae8c"
+        ]
 
         self.makeapirequest.return_value = self.mem_data
 
