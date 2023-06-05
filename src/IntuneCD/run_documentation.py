0a95b9c40f8cbf1d482fc889cb39b59a9b0e5d5d
Aaron Parker <aaron@stealthpuppy.com>
2023-06-05 23:32:35 +1000

    Update run_documentation.py
    Rename value for title from "MEM Documentation" to "Intune Documentation" so that generated documentation has the correct branding

--- src/IntuneCD/run_documentation.py
+++ src/IntuneCD/run_documentation.py
@@ -406,7 +406,7 @@ def start():
             if "title" in json_dict:
                 title = json_dict["title"]
             else:
-                title = "MEM Documentation"
+                title = "Intune Documentation"
             if "intro" in json_dict:
                 intro = json_dict["intro"]
             else:
@@ -420,7 +420,7 @@ def start():
             else:
                 updated = f"**Document updated on:**"
         else:
-            title = "MEM Documentation"
+            title = "Intune Documentation"
             intro = args.intro
             tenant = f"**Tenant:** {tenantname}"
             updated = f"**Document updated on:**"
