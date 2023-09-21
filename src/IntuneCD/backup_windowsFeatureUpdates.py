97ffbc9fb7814b001a993fa845491ea729341dfa
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2023-09-21 10:24:58 +0200

    Create backup_windowsFeatureUpdates.py

---
+++ src/IntuneCD/backup_windowsFeatureUpdates.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+"""
+This module backs up all Windows Feature Update Profiles in Intune.
+"""
+
+from .clean_filename import clean_filename
+from .graph_request import makeapirequest
+from .graph_batch import batch_assignment, get_object_assignment
+from .save_output import save_output
+from .remove_keys import remove_keys
+from .check_prefix import check_prefix_match
+
+# Set MS Graph endpoint
+ENDPOINT = (
+    "https://graph.microsoft.com/beta/deviceManagement/windowsFeatureUpdateProfiles"
+)
+
+
+# Get all Windows Feature Profiles and save them in specified path
+def savebackup(path, output, exclude, token, prefix):
+    """
+    Saves all Windows Feature Update Profiles in Intune to a JSON or YAML file.
+
+    :param path: Path to save the backup to
+    :param output: Format the backup will be saved as
+    :param exclude: If "assignments" is in the list, it will not back up the assignments
+    :param token: Token to use for authenticating the request
+    """
+
+    results = {"config_count": 0, "outputs": []}
+    configpath = path + "/" + "Feature Updates/"
+    data = makeapirequest(ENDPOINT, token)
+
+    assignment_responses = batch_assignment(
+        data,
+        "deviceManagement/windowsFeatureUpdateProfiles/",
+        "/assignments",
+        token,
+    )
+
+    for profile in data["value"]:
+        if prefix and not check_prefix_match(profile["displayName"], prefix):
+            continue
+
+        results["config_count"] += 1
+        if "assignments" not in exclude:
+            assignments = get_object_assignment(profile["id"], assignment_responses)
+            if assignments:
+                profile["assignments"] = assignments
+
+        profile = remove_keys(profile)
+
+        print("Backing up Feature Update profile: " + profile["displayName"])
+
+        # Get filename without illegal characters
+        fname = clean_filename(profile["displayName"])
+        # Save Windows Enrollment Profile as JSON or YAML depending on
+        # configured value in "-o"
+        save_output(output, configpath, fname, profile)
+
+        results["outputs"].append(fname)
+
+    return results
