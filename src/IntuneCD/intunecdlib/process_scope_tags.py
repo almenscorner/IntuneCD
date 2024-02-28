640863621a53f58bc5054d3ae06f0e473b1aa5c4
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2024-02-28 15:18:21 +0100

    look for additional keys

--- src/IntuneCD/intunecdlib/process_scope_tags.py
+++ src/IntuneCD/intunecdlib/process_scope_tags.py
@@ -28,16 +28,20 @@ def get_scope_tags_name(data, scope_tags):
     :param scope_tags: The scope tag to search for
     """
 
-    log("get_scope_tags_name", "Checking if scope tags are in the data.")
-    if data.get("roleScopeTagIds"):
-        log("get_scope_tags_name", "Scope tags are in the data.")
+    def _get_scope_tags(scope_tag_key):
+        log("get_scope_tags_name", "Checking if scope tags are in the data.")
         # list comprehension to get the scope tag name
-        data["roleScopeTagIds"] = [
-            tag["displayName"]
-            for tag in scope_tags
-            if tag["id"] in data["roleScopeTagIds"]
-        ]
-        log("get_scope_tags_name", f"Scope tags: {data['roleScopeTagIds']}")
+        if data.get(scope_tag_key):
+            log("get_scope_tags_name", "Scope tags are in the data.")
+            data[scope_tag_key] = [
+                tag["displayName"]
+                for tag in scope_tags
+                if tag["id"] in data[scope_tag_key]
+            ]
+            log("get_scope_tags_name", f"Scope tags: {data[scope_tag_key]}")
+
+    _get_scope_tags("roleScopeTagIds")
+    _get_scope_tags("roleScopeTags")
 
     return data
 
@@ -50,15 +54,19 @@ def get_scope_tags_id(data, scope_tags):
     :param scope_tags: The scope tag to search for
     """
 
-    log("get_scope_tags_id", "Checking if scope tags are in the data.")
-    if data.get("roleScopeTagIds"):
-        log("get_scope_tags_id", "Scope tags are in the data.")
-        # list comprehension to get the scope tag ID
-        data["roleScopeTagIds"] = [
-            tag["id"]
-            for tag in scope_tags
-            if tag["displayName"] in data["roleScopeTagIds"]
-        ]
-        log("get_scope_tags_id", f"Scope tags: {data['roleScopeTagIds']}")
+    def _get_scope_tags(scope_tag_key):
+        log("get_scope_tags_id", "Checking if scope tags are in the data.")
+        # list comprehension to get the scope tag name
+        if data.get(scope_tag_key):
+            log("get_scope_tags_id", "Scope tags are in the data.")
+            data[scope_tag_key] = [
+                tag["id"]
+                for tag in scope_tags
+                if tag["displayName"] in data[scope_tag_key]
+            ]
+            log("get_scope_tags_id", f"Scope tags: {data[scope_tag_key]}")
+
+    _get_scope_tags("roleScopeTagIds")
+    _get_scope_tags("roleScopeTags")
 
     return data
