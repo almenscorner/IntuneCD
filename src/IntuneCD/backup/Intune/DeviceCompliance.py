d1e1308871066646e35c901b48c79a86b4b7af36
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2024-03-19 11:05:59 +0100

    Move helper function in to class

--- src/IntuneCD/backup/Intune/DeviceCompliance.py
+++ src/IntuneCD/backup/Intune/DeviceCompliance.py
@@ -2,70 +2,6 @@
 from ...intunecdlib.BaseBackupModule import BaseBackupModule
 
 
-def _check_linux_discovery_script(data: dict) -> bool:
-    """Checks if the data is a Linux discovery script
-
-    Args:
-        data (dict): The data to check
-
-    Returns:
-        bool: If the data is a Linux discovery script
-    """
-    if isinstance(data, dict):
-        if "linux_customcompliance_discoveryscript" in data.values():
-            return True
-        return any(_check_linux_discovery_script(v) for v in data.values())
-    if isinstance(data, list):
-        return any(_check_linux_discovery_script(v) for v in data)
-    return False
-
-
-def _get_detection_script_id(data: dict, path=None) -> list:
-    """Gets the detection script ID path from the data
-
-    Args:
-        data (dict): The data to get the detection script ID from
-        path (_type_, optional): The path to the detection script ID
-
-    Returns:
-        list: The path to the detection script ID
-    """
-    if path is None:
-        path = []
-    if isinstance(data, dict):
-        for k, v in data.items():
-            if isinstance(v, dict):
-                new_path = _get_detection_script_id(v, path + [k])
-                if new_path is not None:
-                    return new_path
-            elif isinstance(v, list):
-                for i, item in enumerate(v):
-                    new_path = _get_detection_script_id(item, path + [k, i])
-                    if new_path is not None:
-                        return new_path
-            elif v == "linux_customcompliance_discoveryscript":
-                return path
-    return None
-
-
-def _get_value_from_path(data: dict, path: list) -> str:
-    """Gets the value from the path in the data
-
-    Args:
-        data (dict): The data to get the value from
-        path (list): The path to the value
-
-    Returns:
-        str: The value from the path
-    """
-    for key in path:
-        if isinstance(data, list):
-            data = data[int(key)]
-        else:
-            data = data[key]
-    return data["simpleSettingValue"]["value"]
-
-
 class DeviceComplianceBackupModule(BaseBackupModule):
     """A class used to backup Intune Device Compliance Policies
 
@@ -94,6 +30,91 @@ class DeviceComplianceBackupModule(BaseBackupModule):
         )
         self.assignment_extra_url = self.assignment_extra_url or "/assignments"
 
+    def _check_linux_discovery_script(self, data: dict) -> bool:
+        """Checks if the data is a Linux discovery script
+
+        Args:
+            data (dict): The data to check
+
+        Returns:
+            bool: If the data is a Linux discovery script
+        """
+        if isinstance(data, dict):
+            if "linux_customcompliance_discoveryscript" in data.values():
+                return True
+            return any(self._check_linux_discovery_script(v) for v in data.values())
+        if isinstance(data, list):
+            return any(self._check_linux_discovery_script(v) for v in data)
+        return False
+
+    def _get_detection_script_id(self, data: dict, path=None) -> list:
+        """Gets the detection script ID path from the data
+
+        Args:
+            data (dict): The data to get the detection script ID from
+            path (_type_, optional): The path to the detection script ID
+
+        Returns:
+            list: The path to the detection script ID
+        """
+        if path is None:
+            path = []
+        if isinstance(data, dict):
+            for k, v in data.items():
+                if isinstance(v, dict):
+                    new_path = self._get_detection_script_id(v, path + [k])
+                    if new_path is not None:
+                        return new_path
+                elif isinstance(v, list):
+                    for i, item in enumerate(v):
+                        new_path = self._get_detection_script_id(item, path + [k, i])
+                        if new_path is not None:
+                            return new_path
+                elif v == "linux_customcompliance_discoveryscript":
+                    return path
+        return None
+
+    def _get_value_from_path(self, data: dict, path: list) -> str:
+        """Gets the value from the path in the data
+
+        Args:
+            data (dict): The data to get the value from
+            path (list): The path to the value
+
+        Returns:
+            str: The value from the path
+        """
+        for key in path:
+            if isinstance(data, list):
+                data = data[int(key)]
+            else:
+                data = data[key]
+        return data["simpleSettingValue"]["value"]
+
+    def _get_notification_template(self, rule: dict[str, any]) -> dict[str, any]:
+        """Gets the notification template for a rule
+
+        Args:
+            rule (dict[str, any]): The rule to get the notification template for
+
+        Returns:
+            dict[str, any]: The notification template
+        """
+        for action in rule["scheduledActionConfigurations"]:
+            if (
+                action.get("notificationTemplateId")
+                != "00000000-0000-0000-0000-000000000000"
+            ):
+                notification_template = self.make_graph_request(
+                    self.endpoint
+                    + "/beta/deviceManagement/notificationMessageTemplates/"
+                    + action["notificationTemplateId"]
+                )
+                if notification_template:
+                    action["notificationTemplateName"] = notification_template[
+                        "displayName"
+                    ]
+
     def main(self) -> dict[str, any]:
         """The main method to backup the Device Compliance Policies
 
@@ -115,11 +136,11 @@ class DeviceComplianceBackupModule(BaseBackupModule):
 
         for item in self.graph_data["value"]:
             # Is the policy a Linux discovery script?
-            if _check_linux_discovery_script(item):
+            if self._check_linux_discovery_script(item):
                 # Get the detection script ID
-                detection_script_id_path = _get_detection_script_id(item)
+                detection_script_id_path = self._get_detection_script_id(item)
                 if detection_script_id_path is not None:
-                    detection_script_id = _get_value_from_path(
+                    detection_script_id = self._get_value_from_path(
                         item, detection_script_id_path
                     )
                     # get the script name
@@ -144,6 +165,7 @@ class DeviceComplianceBackupModule(BaseBackupModule):
 
             for action in item["scheduledActionsForRule"]:
                 self.remove_keys(action)
+                self._get_notification_template(action)
             for config in item["scheduledActionsForRule"][0][
                 "scheduledActionConfigurations"
             ]:
