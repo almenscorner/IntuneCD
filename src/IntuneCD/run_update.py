4666178e0f7494b6d646aa8d91598f4b37f10e61
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2024-06-17 15:02:49 +0200

    exit on error

--- src/IntuneCD/run_update.py
+++ src/IntuneCD/run_update.py
@@ -170,11 +170,19 @@ def start():
         help="The authentication token to use for the update if not using an app registration",
         type=str,
     )
+    parser.add_argument(
+        "--exit-on-error",
+        help="When this parameter is set, IntuneCD will exit on error",
+        action="store_true",
+    )
 
     args = parser.parse_args()
 
     if args.verbose:
         os.environ["VERBOSE"] = "True"
+
+    if args.exit_on_error:
+        os.environ["EXIT_ON_ERROR"] = "True"
 
     def devtoprod():
         return "devtoprod"
@@ -315,6 +323,8 @@ def start():
 
     if "VERBOSE" in os.environ:
         del os.environ["VERBOSE"]
+    if "EXIT_ON_ERROR" in os.environ:
+        del os.environ["EXIT_ON_ERROR"]
 
 
 if __name__ == "__main__":
