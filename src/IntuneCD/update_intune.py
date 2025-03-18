d705c3d45a419b06040d3a48c4737e7b786f96cf
Tobias Almén <78877636+almenscorner@users.noreply.github.com>
2025-03-18 17:23:35 +0100

    make modular and threaded

--- src/IntuneCD/update_intune.py
+++ src/IntuneCD/update_intune.py
@@ -1,4 +1,26 @@
 # -*- coding: utf-8 -*-
+import importlib
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+from .intunecdlib.process_scope_tags import ProcessScopeTags
+
+
+def import_update_module(module_path: str):
+    """
+    Dynamically imports a update module, handling both installed and local Git repository cases.
+    """
+    try:
+        # Try importing as an installed package
+        return importlib.import_module(module_path, package="IntuneCD")
+    except ModuleNotFoundError:
+        try:
+            # If that fails, assume we're running locally and try direct relative import
+            return importlib.import_module(module_path)
+        except ModuleNotFoundError as e:
+            print(f"[ERROR] Could not import {module_path}: {e}")
+            return None
+
+
 def update_intune(
     diff_summary,
     path,
@@ -9,10 +31,17 @@ def update_intune(
     remove,
     exclude,
     args,
+    max_workers,
 ):
     """
-    Imports all the update functions and runs them
+    Dynamically imports and runs update modules in parallel.
     """
+
+    process_scope_tags = ProcessScopeTags(token)
+    scope_tags = None
+    if "ScopeTags" not in exclude:
+        scope_tags = process_scope_tags.get_scope_tags()
+
     params = {
         "path": path,
         "token": token,
@@ -21,171 +50,157 @@ def update_intune(
         "remove": remove,
         "create_groups": create_groups,
         "handle_assignment": assignment,
+        "scope_tags": scope_tags,
     }
 
-    from .intunecdlib.process_scope_tags import ProcessScopeTags
-
-    process_scope_tags = ProcessScopeTags(token)
-
-    if "ScopeTags" not in exclude:
-        from .update.Intune.ScopeTags import ScopeTagsUpdateModule
-
-        diff_summary.append(ScopeTagsUpdateModule(**params).main())
-        scope_tags = process_scope_tags.get_scope_tags()
-    else:
-        scope_tags = None
-
-    params["scope_tags"] = scope_tags
-
-    if "Filters" not in exclude:
-        from .update.Intune.Filters import FiltersUpdateModule
-
-        diff_summary.append(FiltersUpdateModule(**params).main())
-
-    if "AppConfigurations" not in exclude:
-        from .update.Intune.AppConfiguration import AppConfigurationUpdateModule
-
-        diff_summary.append(AppConfigurationUpdateModule(**params).main())
-
-    if "AppProtection" not in exclude:
-        from .update.Intune.AppProtection import AppProtectionUpdateModule
-
-        diff_summary.append(AppProtectionUpdateModule(**params).main())
-
-    if "NotificationTemplate" not in exclude:
-        from .update.Intune.NotificationTemplate import NotificationTemplateUpdateModule
-
-        diff_summary.append(NotificationTemplateUpdateModule(**params).main())
-
-    if "ReusablePolicySettings" not in exclude:
-        from .update.Intune.ReusableSettings import ReusableSettingsUpdateModule
-
-        diff_summary.append(ReusableSettingsUpdateModule(**params).main())
-
-    if "ComplianceScripts" not in exclude:
-        from .update.Intune.ComplianceScripts import ComplianceScriptsUpdateModule
-
-        diff_summary.append(ComplianceScriptsUpdateModule(**params).main())
-
-    if "DeviceCompliancePolicies" not in exclude:
-        from .update.Intune.Compliance import ComplianceUpdateModule
-
-        diff_summary.append(ComplianceUpdateModule(**params).main())
-
-    if "Compliance" not in exclude:
-        from .update.Intune.DeviceCompliance import DeviceComplianceUpdateModule
-
-        diff_summary.append(DeviceComplianceUpdateModule(**params).main())
+    update_modules = [
+        ("ScopeTags", ".update.Intune.ScopeTags", "ScopeTagsUpdateModule"),
+        ("Filters", ".update.Intune.Filters", "FiltersUpdateModule"),
+        (
+            "AppConfigurations",
+            ".update.Intune.AppConfiguration",
+            "AppConfigurationUpdateModule",
+        ),
+        ("AppProtection", ".update.Intune.AppProtection", "AppProtectionUpdateModule"),
+        (
+            "NotificationTemplate",
+            ".update.Intune.NotificationTemplate",
+            "NotificationTemplateUpdateModule",
+        ),
+        (
+            "ReusablePolicySettings",
+            ".update.Intune.ReusableSettings",
+            "ReusableSettingsUpdateModule",
+        ),
+        (
+            "ComplianceScripts",
+            ".update.Intune.ComplianceScripts",
+            "ComplianceScriptsUpdateModule",
+        ),
+        (
+            "DeviceCompliancePolicies",
+            ".update.Intune.Compliance",
+            "ComplianceUpdateModule",
+        ),
+        (
+            "Compliance",
+            ".update.Intune.DeviceCompliance",
+            "DeviceComplianceUpdateModule",
+        ),
+        (
+            "DeviceCategories",
+            ".update.Intune.DeviceCategories",
+            "DeviceCategoriesUpdateModule",
+        ),
+        (
+            "Profiles",
+            ".update.Intune.DeviceConfigurations",
+            "DeviceConfigurationsUpdateModule",
+        ),
+        (
+            "GPOConfigurations",
+            ".update.Intune.GroupPolicyConfigurations",
+            "GroupPolicyConfigurationsUpdateModule",
+        ),
+        (
+            "AppleEnrollmentProfile",
+            ".update.Intune.AppleEnrollmentProfile",
+            "AppleEnrollmentProfileUpdateModule",
+        ),
+        (
+            "WindowsEnrollmentProfile",
+            ".update.Intune.WindowsEnrollmentProfile",
+            "WindowsEnrollmentProfileUpdateModule",
+        ),
+        (
+            "EnrollmentStatusPage",
+            ".update.Intune.EnrollmentStatusPage",
+            "EnrollmentStatusPageUpdateModule",
+        ),
+        (
+            "EnrollmentConfigurations",
+            ".update.Intune.EnrollmentConfigurations",
+            "EnrollmentConfigurationsUpdateModule",
+        ),
+        (
+            "Intents",
+            ".update.Intune.ManagementIntents",
+            "ManagementIntentsUpdateModule",
+        ),
+        (
+            "ProactiveRemediation",
+            ".update.Intune.ProactiveRemediation",
+            "ProactiveRemediationUpdateModule",
+        ),
+        (
+            "PowershellScripts",
+            ".update.Intune.PowerShellScripts",
+            "PowerShellScriptsUpdateModule",
+        ),
+        ("ShellScripts", ".update.Intune.ShellScripts", "ShellScriptsUpdateModule"),
+        (
+            "CustomAttribute",
+            ".update.Intune.CustomAttributes",
+            "CustomAttributesUpdateModule",
+        ),
+        (
+            "ConfigurationPolicies",
+            ".update.Intune.SettingsCatalog",
+            "SettingsCatalogUpdateModule",
+        ),
+        (
+            "ConditionalAccess",
+            ".update.Intune.ConditionalAccess",
+            "ConditionalAccessUpdateModule",
+        ),
+        (
+            "WindowsDriverUpdateProfiles",
+            ".update.Intune.WindowsDriverUpdates",
+            "WindowsDriverUpdatesUpdateModule",
+        ),
+        (
+            "windowsFeatureUpdates",
+            ".update.Intune.WindowsFeatureUpdates",
+            "WindowsFeatureUpdatesUpdateModule",
+        ),
+        (
+            "windowsQualityUpdates",
+            ".update.Intune.WindowsQualityUpdates",
+            "WindowsQualityUpdatesUpdateModule",
+        ),
+        ("Roles", ".update.Intune.Roles", "RolesUpdateModule"),
+    ]
 
     if "DeviceManagementSettings" not in exclude and args.interactiveauth is True:
-        from .update.Intune.DeviceManagementSettings import (
-            DeviceManagementSettingsUpdateModule,
+        update_modules.append(
+            (
+                "DeviceManagementSettings",
+                ".update.Intune.DeviceManagementSettings",
+                "DeviceManagementSettingsUpdateModule",
+            )
         )
-
-        diff_summary.append(DeviceManagementSettingsUpdateModule(**params).main())
     else:
         print(
             "***Device Management Settings is only available with interactive auth***"
         )
 
-    if "DeviceCategories" not in exclude:
-        from .update.Intune.DeviceCategories import DeviceCategoriesUpdateModule
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_module = {}
 
-        diff_summary.append(DeviceCategoriesUpdateModule(**params).main())
+        for exclude_key, module_path, class_name in update_modules:
+            if exclude_key not in exclude:
+                module = import_update_module(module_path)
+                if module:
+                    update_class = getattr(module, class_name)
+                    future = executor.submit(update_class(**params).main)
+                    future_to_module[future] = exclude_key
 
-    if "Profiles" not in exclude:
-        from .update.Intune.DeviceConfigurations import DeviceConfigurationsUpdateModule
-
-        diff_summary.append(DeviceConfigurationsUpdateModule(**params).main())
-
-    if "GPOConfigurations" not in exclude:
-        from .update.Intune.GroupPolicyConfigurations import (
-            GroupPolicyConfigurationsUpdateModule,
-        )
-
-        diff_summary.append(GroupPolicyConfigurationsUpdateModule(**params).main())
-
-    if "AppleEnrollmentProfile" not in exclude:
-        from .update.Intune.AppleEnrollmentProfile import (
-            AppleEnrollmentProfileUpdateModule,
-        )
-
-        diff_summary.append(AppleEnrollmentProfileUpdateModule(**params).main())
-
-    if "WindowsEnrollmentProfile" not in exclude:
-        from .update.Intune.WindowsEnrollmentProfile import (
-            WindowsEnrollmentProfileUpdateModule,
-        )
-
-        diff_summary.append(WindowsEnrollmentProfileUpdateModule(**params).main())
-
-    if "EnrollmentStatusPage" not in exclude:
-        from .update.Intune.EnrollmentStatusPage import EnrollmentStatusPageUpdateModule
-
-        diff_summary.append(EnrollmentStatusPageUpdateModule(**params).main())
-
-    if "EnrollmentConfigurations" not in exclude:
-        from .update.Intune.EnrollmentConfigurations import (
-            EnrollmentConfigurationsUpdateModule,
-        )
-
-        diff_summary.append(EnrollmentConfigurationsUpdateModule(**params).main())
-
-    if "Intents" not in exclude:
-        from .update.Intune.ManagementIntents import ManagementIntentsUpdateModule
-
-        diff_summary.append(ManagementIntentsUpdateModule(**params).main())
-
-    if "ProactiveRemediation" not in exclude:
-        from .update.Intune.ProactiveRemediation import ProactiveRemediationUpdateModule
-
-        diff_summary.append(ProactiveRemediationUpdateModule(**params).main())
-
-    if "PowershellScripts" not in exclude:
-        from .update.Intune.PowerShellScripts import PowerShellScriptsUpdateModule
-
-        diff_summary.append(PowerShellScriptsUpdateModule(**params).main())
-
-    if "ShellScripts" not in exclude:
-        from .update.Intune.ShellScripts import ShellScriptsUpdateModule
-
-        diff_summary.append(ShellScriptsUpdateModule(**params).main())
-
-    if "CustomAttribute" not in exclude:
-        from .update.Intune.CustomAttributes import CustomAttributesUpdateModule
-
-        diff_summary.append(CustomAttributesUpdateModule(**params).main())
-
-    if "ConfigurationPolicies" not in exclude:
-        from .update.Intune.SettingsCatalog import SettingsCatalogUpdateModule
-
-        diff_summary.append(SettingsCatalogUpdateModule(**params).main())
-
-    if "ConditionalAccess" not in exclude:
-        from .update.Intune.ConditionalAccess import ConditionalAccessUpdateModule
-
-        diff_summary.append(ConditionalAccessUpdateModule(**params).main())
-
-    if "WindowsDriverUpdateProfiles" not in exclude:
-        from .update.Intune.WindowsDriverUpdates import WindowsDriverUpdatesUpdateModule
-
-        diff_summary.append(WindowsDriverUpdatesUpdateModule(**params).main())
-
-    if "windowsFeatureUpdates" not in exclude:
-        from .update.Intune.WindowsFeatureUpdates import (
-            WindowsFeatureUpdatesUpdateModule,
-        )
-
-        diff_summary.append(WindowsFeatureUpdatesUpdateModule(**params).main())
-
-    if "windowsQualityUpdates" not in exclude:
-        from .update.Intune.WindowsQualityUpdates import (
-            WindowsQualityUpdatesUpdateModule,
-        )
-
-        diff_summary.append(WindowsQualityUpdatesUpdateModule(**params).main())
-
-    if "Roles" not in exclude:
-        from .update.Intune.Roles import RolesUpdateModule
-
-        diff_summary.append(RolesUpdateModule(**params).main())
+        # Collect results as tasks complete
+        for future in as_completed(future_to_module):
+            module_name = future_to_module[future]
+            try:
+                result = future.result()
+                if result:
+                    diff_summary.append(result)
+            except Exception as e:
+                print(f"[ERROR] {module_name} failed with exception: {e}")
